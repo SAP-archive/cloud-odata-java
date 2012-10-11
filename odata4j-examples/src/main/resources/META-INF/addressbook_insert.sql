d3c52c6b534820db8bef60a6c642986560bac071
Christian Amend <christian.amend@sap.com>
2012-10-11 09:05:26 +0200

    Deleted Files
    Change-Id: I9a0ffa840a6c151c5ab0626ad80db7a8cfe499b0

--- odata4j-examples/src/main/resources/META-INF/addressbook_insert.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM Persons
-INSERT INTO `Persons`(`PersonId`,`Name`,`EmailAddress`,`BirthDay`) VALUES(1,'Susan Summer','susan@private-domain.net','1975-07-22');
-INSERT INTO `Persons`(`PersonId`,`Name`,`EmailAddress`,`BirthDay`) VALUES(2,'Walter Winter','walter.winter@company.com','1968-01-13');
-INSERT INTO `Persons`(`PersonId`,`Name`,`EmailAddress`,`BirthDay`) VALUES(3,'Frederic Fall','ff@some-organisation.org','1983-10-31');
--- odata4j-jersey/src/test/java/org/odata4j/test/unit/jersey/PlaceHolder.java
+++
@@ -1,9 +0,0 @@
-package org.odata4j.test.unit.jersey;
-
-import org.junit.Test;
-
-public class PlaceHolder {
-
-  @Test
-  public void dummy() throws Exception {}
-}
