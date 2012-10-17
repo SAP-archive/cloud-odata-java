fd17f027b58719c8768aaaee2fef28ce5bbb466c
Stephan Klevenz <stephan.klevenz@sap.com>
2012-10-17 09:41:04 +0200

    ODataExceptionMapper
    Change-Id: I61c5601f7cee893976d4077351bd22e65418ee90

---
+++ odata-core/src/main/java/com/sap/core/odata/core/rest/impl/ODataExceptionMapper.java
@@ -0,0 +1,33 @@
+package com.sap.core.odata.core.rest.impl;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Provider
+public class ODataExceptionMapper implements ExceptionMapper<RuntimeException> {
+
+  private final static Logger log = LoggerFactory.getLogger(ODataExceptionMapper.class);
+
+  @Override
+  public Response toResponse(RuntimeException exception) {
+
+    String entity;
+    Status status;
+
+    try {
+      throw exception;
+    } catch (RuntimeException e) {
+      ODataExceptionMapper.log.error(e.getMessage(), e);
+      entity = exception.getClass().getCanonicalName() + " - " + exception.getMessage();
+      status = Status.INTERNAL_SERVER_ERROR;
+    }
+
+    return Response.status(status).entity(entity).build();
+  }
+
+}
--- odata-fit/src/test/java/com/sap/core/odata/core/fit/basic/test/ErrorResponseTest.java
+++ odata-fit/src/test/java/com/sap/core/odata/core/fit/basic/test/ErrorResponseTest.java
@@ -2,33 +2,52 @@ package com.sap.core.odata.core.fit.basic.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.net.URI;
 
+import javax.ws.rs.core.Response;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
-import org.odata4j.exceptions.NotFoundException;
 
-import com.sap.core.odata.core.producer.Entity;
+import com.sap.core.odata.core.edm.Edm;
+import com.sap.core.odata.core.edm.EdmEntityContainer;
+import com.sap.core.odata.core.edm.EdmEntitySet;
+import com.sap.core.odata.core.producer.EntitySet;
 import com.sap.core.odata.fit.StringStreamHelper;
 
 public class ErrorResponseTest extends AbstractBasicTest {
 
+  @Before
+  public void before() throws Exception {
+    super.before();
+
+    EdmEntitySet edmEntitySet = mock(EdmEntitySet.class);
+    EdmEntityContainer edmEntityContainer = mock(EdmEntityContainer.class);
+    when(edmEntityContainer.getEntitySet("entityset")).thenReturn(edmEntitySet);
+    Edm edm = this.getProducer().getMetadata().getEdm();
+    when(edm.getDefaultEntityContainer()).thenReturn(edmEntityContainer);
+    EntitySet entitySet = this.getProducer().getEntitySet();
+    when(entitySet.read()).thenReturn(Response.ok().entity("entityset").build());
+  }
+
+  
   @Test
-  @Ignore
-  public void test404NotFound() throws ClientProtocolException, IOException {
+  public void test500RuntimeError() throws ClientProtocolException, IOException {
 
-    Entity entityProducer = this.getProducer().getEntity();
+    EntitySet entitySetProducer = this.getProducer().getEntitySet();
 
-    doThrow(new NotFoundException("Buhh")).when(entityProducer).read();
+    doThrow(new RuntimeException("Bumms")).when(entitySetProducer).read();
 
-    HttpGet get = new HttpGet(URI.create(this.getEndpoint().toString() + "xyz"));
+    HttpGet get = new HttpGet(URI.create(this.getEndpoint().toString() + "entityset"));
     HttpResponse response = this.getHttpClient().execute(get);
-    assertEquals(404, response.getStatusLine().getStatusCode());
+    assertEquals(500, response.getStatusLine().getStatusCode());
 
     this.log.debug(StringStreamHelper.inputStreamToString(response.getEntity().getContent()));
   }
