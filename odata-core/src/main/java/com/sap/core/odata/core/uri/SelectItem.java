316a90974518075dc96d0b39142910ee7fca5bef
Klaus Straubinger <klaus.straubinger@sap.com>
2012-10-17 10:09:31 +0200

    change $select parsing to string splitting + clean-ups
    Change-Id: I2744dd8ae69185a3e7f11730acee0e9f58180e7b

--- odata-core/src/main/java/com/sap/core/odata/core/uri/SelectItem.java
+++ odata-core/src/main/java/com/sap/core/odata/core/uri/SelectItem.java
@@ -6,38 +6,37 @@ import java.util.List;
 
 import com.sap.core.odata.core.edm.EdmTyped;
 
-
-
 public class SelectItem {
 
   private List<NavigationPropertySegment> navigationPropertySegments = Collections.emptyList();
   private EdmTyped property;
   private boolean star;
-  
+
   public boolean isStar() {
     return star;
   }
+
   public void setStar(boolean star) {
     this.star = star;
   }
+
   public EdmTyped getProperty() {
     return property;
   }
+
   public void setProperty(EdmTyped property) {
     this.property = property;
   }
-  
-  public void addNavigationPropertySegment(NavigationPropertySegment segment){
+
+  public void addNavigationPropertySegment(NavigationPropertySegment segment) {
     if (navigationPropertySegments.equals(Collections.EMPTY_LIST))
       navigationPropertySegments = new ArrayList<NavigationPropertySegment>();
 
     navigationPropertySegments.add(segment);
   }
-  
+
   public List<NavigationPropertySegment> getNavigationPropertySegments() {
     return navigationPropertySegments;
   }
-  
-  
-  
+
 }
