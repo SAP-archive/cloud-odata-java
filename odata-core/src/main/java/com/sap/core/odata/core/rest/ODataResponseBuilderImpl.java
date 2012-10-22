c1b5f0efbedf75ed36eedb621a4ca787368ca759
Stephan Klevenz <stephan.klevenz@sap.com>
2012-10-22 10:41:20 +0200

    ODataResponse
    Change-Id: I1e301637b7e4c932013431fb22d0f3088d18d307

--- odata-core/src/main/java/com/sap/core/odata/core/rest/ODataResponseBuilderImpl.java
+++ odata-core/src/main/java/com/sap/core/odata/core/rest/ODataResponseBuilderImpl.java
@@ -1,17 +1,24 @@
 package com.sap.core.odata.core.rest;
 
+import java.util.HashMap;
+
 import com.sap.core.odata.api.rest.ODataResponse;
 import com.sap.core.odata.api.rest.ODataResponse.ODataResponseBuilder;
 
 public class ODataResponseBuilderImpl extends ODataResponseBuilder {
 
-  private int status;
-
+  private int status = 200;
+  private String entity;
+  private HashMap<String, String> header = new HashMap<String, String>();
+  
+  
   @Override
   public ODataResponse build() {
     ODataResponseImpl response = new ODataResponseImpl();
 
     response.setStatus(this.status);
+    response.setEntity(this.entity);
+    response.setHeader(this.header);
     
     return response;
   }
@@ -19,6 +26,23 @@ public class ODataResponseBuilderImpl extends ODataResponseBuilder {
   @Override
   public ODataResponseBuilder status(int status) {
     this.status = status;
+    return this;
+  }
+
+  @Override
+  public ODataResponseBuilder entity(String entity) {
+    this.entity= entity;
+    return this;
+  }
+
+  @Override
+  public ODataResponseBuilder header(String name, String value) {
+    if (value == null) {
+      this.header.remove(name);
+    } else {
+      this.header.put(name, value);
+    }
+    
     return this;
   }
 
