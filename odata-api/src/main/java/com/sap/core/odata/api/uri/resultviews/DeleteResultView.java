b0d2ec6143d1d08d3d8e06173501675fac829aaa
Christian Amend <christian.amend@sap.com>
2012-10-25 16:47:40 +0200

    Introduce uri result views put post merge patch...
    Change-Id: Id35818180546f59df7d1267e0a89caa2bd30d326

---
+++ odata-api/src/main/java/com/sap/core/odata/api/uri/resultviews/DeleteResultView.java
@@ -0,0 +1,77 @@
+package com.sap.core.odata.api.uri.resultviews;
+
+import java.util.List;
+import java.util.Map;
+
+import com.sap.core.odata.api.edm.EdmEntityContainer;
+import com.sap.core.odata.api.edm.EdmEntitySet;
+import com.sap.core.odata.api.edm.EdmFunctionImport;
+import com.sap.core.odata.api.edm.EdmProperty;
+import com.sap.core.odata.api.edm.EdmType;
+import com.sap.core.odata.api.enums.UriType;
+import com.sap.core.odata.api.uri.KeyPredicate;
+import com.sap.core.odata.api.uri.NavigationSegment;
+import com.sap.core.odata.api.uri.UriLiteral;
+
+public interface DeleteResultView {
+
+  /**
+   * @return {@link UriType} the uri type
+   */
+  public UriType getUriType();
+
+  /**
+   * @return {@link EdmEntityContainer} the target entity container
+   */
+  public EdmEntityContainer getEntityContainer();
+
+  /**
+   * @return {@link EdmEntitySet}
+   */
+  public EdmEntitySet getStartEntitySet();
+
+  /**
+   * @return {@link EdmEntitySet} target entity set
+   */
+  public EdmEntitySet getTargetEntitySet();
+
+  /**
+   * @return {@link EdmFunctionImport} the funktion import
+   */
+  public EdmFunctionImport getFunctionImport();
+
+  /**
+   * @return {@link EdmType} the target type of the entity set
+   */
+  public EdmType getTargetType();
+
+  /**
+   * @return list of {@link KeyPredicate}
+   */
+  public List<KeyPredicate> getKeyPredicates();
+
+  /**
+   * @return list of {@link NavigationSegment}
+   */
+  public List<NavigationSegment> getNavigationSegments();
+
+  /**
+   * @return list of {@link EdmProperty}
+   */
+  public List<EdmProperty> getPropertyPath();
+
+  /**
+   * @return String the customer format
+   */
+  public String getCustomFormat();
+
+  /**
+   * @return Map of {@literal <String,} {@link UriLiteral}{@literal >} function import parameters
+   */
+  public Map<String, UriLiteral> getFunctionImportParameters();
+
+  /**
+   * @return Map of {@literal<String, String>} custom query options
+   */
+  public Map<String, String> getCustomQueryOptions();
+}
