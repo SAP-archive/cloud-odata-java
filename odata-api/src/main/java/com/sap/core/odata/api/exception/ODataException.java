4f2cc1c5caf70a4d56a57ffb5092944369b65817
Michael Bolz <michael.bolz@sap.com>
2012-11-05 14:30:36 +0100

    Add javadoc.
    Change-Id: I1fad80ccba986055bf07d72542b0959f52f50fa7

--- odata-api/src/main/java/com/sap/core/odata/api/exception/ODataException.java
+++ odata-api/src/main/java/com/sap/core/odata/api/exception/ODataException.java
@@ -1,5 +1,8 @@
 package com.sap.core.odata.api.exception;
 
+/**
+ * Common checked exception for <code>OData</code> library and base exception for all <code>OData</code> related exceptions.
+ */
 public class ODataException extends Exception {
 
   private static final long serialVersionUID = 1L;
@@ -20,17 +23,23 @@ public class ODataException extends Exception {
     super(e);
   }
 
-  public boolean causedByContextedException() {
-    return getContextedCause() != null;
+  /**
+   * Check whether this exception was caused by a {@link ODataMessageException} exception.
+   * 
+   * @return <code>true</code> if it was caused by an {@link ODataMessageException}, otherwise <code>false</code>.
+   */
+  public boolean isCausedByMessageException() {
+    return getMessageExceptionCause() != null;
   }
 
   /**
    * Search for and return first (from top) {@link ODataMessageException} in cause hierarchy.
-   * If no {@link ODataMessageException}  in cause hierarchy <code>NULL</code> is returned. 
+   * If no {@link ODataMessageException} in cause hierarchy <code>NULL</code> is returned. 
    * 
-   * @return
+   * @return the first found {@link ODataMessageException} in the cause exception hierarchy. 
+   *          Or <code>NULL</code> if no {@link ODataMessageException} is found in cause hierarchy.
    */
-  public ODataMessageException getContextedCause() {
+  public ODataMessageException getMessageExceptionCause() {
     Throwable cause = getCause();
     while (cause != null) {
       if (cause instanceof ODataMessageException) {
--- odata-core/src/test/java/com/sap/core/odata/core/exception/ODataExceptionTest.java
+++ odata-core/src/test/java/com/sap/core/odata/core/exception/ODataExceptionTest.java
@@ -14,21 +14,21 @@ public class ODataExceptionTest {
   public void testNoCause() {
     ODataException exception = new ODataException("Some message.");
 
-    Assert.assertEquals(false, exception.causedByContextedException());
+    Assert.assertEquals(false, exception.isCausedByMessageException());
   }
 
   @Test
   public void testNPECause() {
     ODataException exception = new ODataException("Some message.", new NullPointerException());
 
-    Assert.assertEquals(false, exception.causedByContextedException());
+    Assert.assertEquals(false, exception.isCausedByMessageException());
   }
 
   @Test
   public void testODataContextedCause() {
     ODataException exception = new ODataException("Some message.", new ODataNotFoundException(ODataNotFoundException.ENTITY));
 
-    Assert.assertEquals(true, exception.causedByContextedException());
+    Assert.assertEquals(true, exception.isCausedByMessageException());
   }
 
   @Test
@@ -38,6 +38,6 @@ public class ODataExceptionTest {
             new MethodNotAllowedException(
                 new ODataNotFoundException(ODataNotFoundException.ENTITY))));
 
-    Assert.assertEquals(true, exception.causedByContextedException());
+    Assert.assertEquals(true, exception.isCausedByMessageException());
   }
 }
