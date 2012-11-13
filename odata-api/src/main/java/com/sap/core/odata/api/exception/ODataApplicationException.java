fb13db7e6c4f9ef396194b2fbe2406da3542aeed
Michael Bolz <michael.bolz@sap.com>
2012-11-13 11:41:48 +0100

    Added new 'ODataApplicationException'.
    Change-Id: I4c11f95cc98ee9820f6b525bb2637246fa090184

---
+++ odata-api/src/main/java/com/sap/core/odata/api/exception/ODataApplicationException.java
@@ -0,0 +1,9 @@
+package com.sap.core.odata.api.exception;
+
+public class ODataApplicationException extends ODataException {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+}
