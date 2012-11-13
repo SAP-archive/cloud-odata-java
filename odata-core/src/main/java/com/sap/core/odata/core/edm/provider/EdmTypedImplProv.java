d322803d80955f664fe7bd24709dbd33ff68d5b9
Klaus Straubinger <klaus.straubinger@sap.com>
2012-11-13 15:44:22 +0100

    more EDM provider implementation
    Change-Id: I2305a06a42b4f639f2481e27b492c2b38d3546fb

---
+++ odata-core/src/main/java/com/sap/core/odata/core/edm/provider/EdmTypedImplProv.java
@@ -0,0 +1,49 @@
+package com.sap.core.odata.core.edm.provider;
+
+import com.sap.core.odata.api.edm.EdmException;
+import com.sap.core.odata.api.edm.EdmMultiplicity;
+import com.sap.core.odata.api.edm.EdmSimpleTypeFacade;
+import com.sap.core.odata.api.edm.EdmSimpleTypeKind;
+import com.sap.core.odata.api.edm.EdmType;
+import com.sap.core.odata.api.edm.EdmTyped;
+import com.sap.core.odata.api.edm.FullQualifiedName;
+
+/**
+ * @author SAP AG
+ */
+public class EdmTypedImplProv extends EdmNamedImplProv implements EdmTyped {
+
+  private EdmType edmType;
+  private FullQualifiedName typeName;
+  private EdmMultiplicity multiplicity;
+
+  public EdmTypedImplProv(EdmImplProv edm, String name, FullQualifiedName typeName, EdmMultiplicity multiplicity) throws EdmException {
+    super(edm, name);
+    this.typeName = typeName;
+    this.multiplicity = multiplicity;
+  }
+
+  @Override
+  public EdmType getType() throws EdmException {
+    if (edmType == null) {
+      final String namespace = typeName.getNamespace();
+      if (EdmSimpleTypeFacade.edmNamespace.equals(typeName.getNamespace())) {
+        edmType = EdmSimpleTypeFacade.getInstance(EdmSimpleTypeKind.valueOf(typeName.getName()));
+      } else {
+        try {
+          edmType = edm.getEntityType(namespace, typeName.getName());
+        } catch (EdmException e) {
+          edmType = null;
+        }
+        if (edmType == null)
+          edmType = edm.getComplexType(namespace, typeName.getName());
+      }
+    }
+    return edmType;
+  }
+
+  @Override
+  public EdmMultiplicity getMultiplicity() throws EdmException {
+    return multiplicity;
+  }
+}
