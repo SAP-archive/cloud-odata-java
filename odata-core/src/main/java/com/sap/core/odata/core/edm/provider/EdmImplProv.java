df9f431d6a4d44adedb16f38c232da08b5238da4
Christian Amend <christian.amend@sap.com>
2012-11-19 13:11:05 +0100

    Start of metadataimplementation
    Change-Id: Id21930c7b6c6e25e3819bd79194aa9d6da6ae3b0

--- odata-core/src/main/java/com/sap/core/odata/core/edm/provider/EdmImplProv.java
+++ odata-core/src/main/java/com/sap/core/odata/core/edm/provider/EdmImplProv.java
@@ -1,8 +1,5 @@
 package com.sap.core.odata.core.edm.provider;
 
-import java.util.Collection;
-
-import com.sap.core.odata.api.edm.Edm;
 import com.sap.core.odata.api.edm.EdmAssociation;
 import com.sap.core.odata.api.edm.EdmComplexType;
 import com.sap.core.odata.api.edm.EdmEntityContainer;
@@ -11,9 +8,6 @@ import com.sap.core.odata.api.edm.EdmException;
 import com.sap.core.odata.api.edm.EdmServiceMetadata;
 import com.sap.core.odata.api.edm.FullQualifiedName;
 import com.sap.core.odata.api.edm.provider.EdmProvider;
-import com.sap.core.odata.api.edm.provider.EntityType;
-import com.sap.core.odata.api.edm.provider.Property;
-import com.sap.core.odata.api.edm.provider.Schema;
 import com.sap.core.odata.api.exception.ODataException;
 import com.sap.core.odata.core.edm.EdmImpl;
 
@@ -50,130 +44,21 @@ public class EdmImplProv extends EdmImpl implements EdmServiceMetadata {
 
   @Override
   public String getMetadata() throws EdmException {
-    //Catch exception here to transform it into an edm exception?
-    
-    Collection<Schema> schemas;
+    Metadata metadata = new Metadata();
     try {
-      schemas = edmProvider.getSchemas();
+      metadata.setSchemas(edmProvider.getSchemas());
     } catch (ODataException e) {
       throw new EdmException(EdmException.COMMON, e);
     }
-
-    if (this.dataServiceVersion == 0) {
-
-      this.dataServiceVersion = Edm.DATA_SERVICE_VERSION10;
-
-      for (Schema schema : schemas) {
-        for (EntityType entityType : schema.getEntityTypes()) {
-
-          if (entityType.getCustomizableFeedMappings().getFcKeepInContent()) {
-            dataServiceVersion = Edm.DATA_SERVICE_VERSION20;
-            break;
-          }
-
-          for (Property property : entityType.getProperties()) {
-            if (property.getCustomizableFeedMappings().getFcKeepInContent()) {
-              dataServiceVersion = Edm.DATA_SERVICE_VERSION20;
-              break;
-            }
-          }
-
-          if (dataServiceVersion == Edm.DATA_SERVICE_VERSION20)
-            break;
-        }
-
-        if (dataServiceVersion == Edm.DATA_SERVICE_VERSION20)
-          break;
-      }
-    }
+    metadata.setDataServiceVersion(this.dataServiceVersion);
     //TODO: Convert Metadata into the right format
-    return "" + dataServiceVersion + schemas;
+    return metadata.toString();
 
-    //    DATA:
-    //      ls_metadata TYPE /IWCOR/if_DS_edm_provider=>data_services_s,
-    //      lo_writer TYPE REF TO cl_sxml_string_writer,
-    //      ld_schema TYPE REF TO /IWCOR/if_DS_edm_provider=>schema_s,
-    //      ld_entity_type TYPE REF TO /IWCOR/if_DS_edm_provider=>entity_type_s,
-    //      ld_property TYPE REF TO /IWCOR/if_DS_edm_provider=>property_s,
-    //      lv_data_service_version type string,
-    //      lo_error_message TYPE REF TO /IWCOR/cx_DS_error_message.
-    //
-    //    TRY.
-    //        mo_provider->get_schemas( IMPORTING et_schema = ls_metadata-schemas ).
-    //      CATCH /IWCOR/cx_DS_error_message INTO lo_error_message.
-    //        RAISE EXCEPTION TYPE /IWCOR/cx_DS_edm_error
-    //          EXPORTING
-    //            previous = lo_error_message.
-    //    ENDTRY.
-    //
-    //    lv_data_service_version = /IWCOR/if_DS_edm=>gc_data_service_version_10.
-    //    LOOP AT ls_metadata-schemas REFERENCE INTO ld_schema.
-    //      LOOP AT ld_schema->entity_types REFERENCE INTO ld_entity_type.
-    //        LOOP AT ld_entity_type->properties REFERENCE INTO ld_property WHERE customizable_feed_mappings-fc_keep_in_content = /IWCOR/if_DS_edm=>gc_bool_false.
-    //          lv_data_service_version = /IWCOR/if_DS_edm=>gc_data_service_version_20.
-    //          EXIT.
-    //        ENDLOOP.
-    //        IF ld_entity_type->customizable_feed_mappings-fc_keep_in_content = /IWCOR/if_DS_edm=>gc_bool_false.
-    //          lv_data_service_version = /IWCOR/if_DS_edm=>gc_data_service_version_20.
-    //        ENDIF.
-    //        IF lv_data_service_version = /IWCOR/if_DS_edm=>gc_data_service_version_20.
-    //          EXIT.
-    //        ENDIF.
-    //      ENDLOOP.
-    //      IF lv_data_service_version = /IWCOR/if_DS_edm=>gc_data_service_version_20.
-    //        EXIT.
-    //      ENDIF.
-    //    ENDLOOP.
-    //    ls_metadata-data_service_version = lv_data_service_version.
-    //
-    //    lo_writer = cl_sxml_string_writer=>create( ).
-    //    /IWCOR/cl_DS_edm_metadata=>write_metadata( is_metadata = ls_metadata io_writer = lo_writer ).
-    //    ev_metadata = lo_writer->get_output( ).
-    //    ev_data_service_version = ls_metadata-data_service_version.
   }
 
   @Override
-  public String getDataServiceVersion() throws EdmException{
-    // cache it, when get metadate is called first, otherwise calculateDataServiceVersion in separate method,
-    // the method is required in proc single to set header "DataServiceVersion" 
-    if (this.dataServiceVersion == 0) {
-      this.dataServiceVersion = calculateDataServiceVersion();
-    }
+  public String getDataServiceVersion() throws EdmException {
+
     return "" + this.dataServiceVersion;
-  }
-
-  private double calculateDataServiceVersion() throws EdmException {
-    double dataServiceVersion = Edm.DATA_SERVICE_VERSION10;
-    
-    Collection<Schema> schemas;
-    try {
-      schemas = edmProvider.getSchemas();
-    } catch (ODataException e) {
-     throw new EdmException(EdmException.COMMON,e);
-    }
-    
-    for (Schema schema : schemas) {
-      for (EntityType entityType : schema.getEntityTypes()) {
-
-        if (entityType.getCustomizableFeedMappings().getFcKeepInContent()) {
-          dataServiceVersion = Edm.DATA_SERVICE_VERSION20;
-          break;
-        }
-
-        for (Property property : entityType.getProperties()) {
-          if (property.getCustomizableFeedMappings().getFcKeepInContent()) {
-            dataServiceVersion = Edm.DATA_SERVICE_VERSION20;
-            break;
-          }
-        }
-
-        if (dataServiceVersion == Edm.DATA_SERVICE_VERSION20)
-          break;
-      }
-
-      if (dataServiceVersion == Edm.DATA_SERVICE_VERSION20)
-        break;
-    }
-    return dataServiceVersion;
   }
 }
