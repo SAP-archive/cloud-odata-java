2719abb58af25b889f70981b63c6ea385e8eb050
Jens Huesken <jens.huesken@sap.com>
2012-11-22 17:01:33 +0100

    Preliminary version of $metadata serialization
    Change-Id: I9c6184b95ce746bf9d72dfd36dfc03275d132008

--- odata-core/src/main/java/com/sap/core/odata/core/edm/provider/EdmMetadata.java
+++ odata-core/src/main/java/com/sap/core/odata/core/edm/provider/EdmMetadata.java
@@ -1,38 +1,105 @@
 package com.sap.core.odata.core.edm.provider;
 
-import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.Collection;
 
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import com.sap.core.odata.api.edm.provider.Association;
+import com.sap.core.odata.api.edm.provider.ComplexType;
 import com.sap.core.odata.api.edm.provider.DataServices;
+import com.sap.core.odata.api.edm.provider.EntityContainer;
+import com.sap.core.odata.api.edm.provider.EntityType;
+import com.sap.core.odata.api.edm.provider.Schema;
 import com.sap.core.odata.api.exception.ODataSerializationException;
 
 public class EdmMetadata {
 
+  private static String EDMX_NAMESPACE = "http://schemas.microsoft.com/ado/2007/06/edmx";
+  private static String METADATA_NAMESPACE = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata";
+  private static String EDM_NAMESPACE = "http://schemas.microsoft.com/ado/2008/09/edm";
+
   //TODO Implement + Exception handling
   public static void writeMetadata(DataServices metadata, Writer writer) throws ODataSerializationException {
+
     try {
-      writer.append('<');
-      writer.append('x');
-      writer.append('m');
-      writer.append('l');
-      writer.append('>');
-      writer.append('M');
-      writer.append('e');
-      writer.append('t');
-      writer.append('a');
-      writer.append('d');
-      writer.append('a');
-      writer.append('t');
-      writer.append('a');
-      writer.append('<');
-      writer.append('/');
-      writer.append('x');
-      writer.append('m');
-      writer.append('l');
-      writer.append('>');
-    } catch (IOException e) {
-      new ODataSerializationException(ODataSerializationException.COMMON, e);
+      XMLStreamWriter xmlStreamWriter = XMLOutputFactory.newInstance().createXMLStreamWriter(writer);
+
+      xmlStreamWriter.writeStartDocument();
+      xmlStreamWriter.setPrefix("edmx", EDMX_NAMESPACE);
+      xmlStreamWriter.setPrefix("m", METADATA_NAMESPACE);
+      xmlStreamWriter.setDefaultNamespace(EDM_NAMESPACE);
+
+      xmlStreamWriter.writeStartElement(EDMX_NAMESPACE, "Edmx");
+      xmlStreamWriter.writeAttribute("Version", "1.0");
+      xmlStreamWriter.writeNamespace("edmx", EDMX_NAMESPACE);
+
+      xmlStreamWriter.writeStartElement(METADATA_NAMESPACE, "DataServices");
+      xmlStreamWriter.writeAttribute("DataServiceVersion", metadata.getDataServiceVersion());
+      xmlStreamWriter.writeNamespace("m", METADATA_NAMESPACE);
+
+      Collection<Schema> schemas = metadata.getSchemas();
+      if (schemas != null) {
+        for (Schema schema : schemas) {
+          xmlStreamWriter.writeStartElement("Schema");
+          xmlStreamWriter.writeAttribute("Namespace", schema.getNamespace());
+          xmlStreamWriter.writeDefaultNamespace(EDM_NAMESPACE);
+
+          Collection<EntityType> entityTypes = schema.getEntityTypes();
+          if (entityTypes != null) {
+            for (EntityType entityType : entityTypes) {
+              xmlStreamWriter.writeStartElement("EntityType");
+              xmlStreamWriter.writeAttribute("Name", entityType.getName());
+              xmlStreamWriter.writeEndElement();
+            }
+          }
+
+          Collection<ComplexType> complexTypes = schema.getComplexTypes();
+          if (complexTypes != null) {
+            for (ComplexType complexType : complexTypes) {
+              xmlStreamWriter.writeStartElement("ComplexType");
+              xmlStreamWriter.writeAttribute("Name", complexType.getName());
+              xmlStreamWriter.writeEndElement();
+            }
+          }
+
+          Collection<Association> associations = schema.getAssociations();
+          if (associations != null) {
+            for (Association association : associations) {
+              xmlStreamWriter.writeStartElement("Association");
+              xmlStreamWriter.writeAttribute("Name", association.getName());
+              xmlStreamWriter.writeEndElement();
+            }
+          }
+
+          Collection<EntityContainer> entityContainers = schema.getEntityContainers();
+          if (entityContainers != null) {
+            for (EntityContainer entityContainer : entityContainers) {
+              xmlStreamWriter.writeStartElement("EntityContainer");
+              xmlStreamWriter.writeAttribute("Name", entityContainer.getName());
+              xmlStreamWriter.writeEndElement();
+            }
+          }
+
+          xmlStreamWriter.writeEndElement();
+        }
+      }
+
+      xmlStreamWriter.writeEndElement();
+      xmlStreamWriter.writeEndElement();
+      xmlStreamWriter.writeEndDocument();
+
+      xmlStreamWriter.flush();
+    } catch (XMLStreamException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    } catch (FactoryConfigurationError e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
     }
   }
 
