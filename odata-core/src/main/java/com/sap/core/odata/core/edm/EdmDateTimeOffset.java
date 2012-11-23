e863cf22ded290d2ba5d65f2fc69fd06530548ec
Klaus Straubinger <klaus.straubinger@sap.com>
2012-11-23 17:00:02 +0100

    Edm.DateTimeOffset valueToString
    Change-Id: I66975bdf7c279bdf9415e7e4e2bfa0ebfc1f96da

--- odata-core/src/main/java/com/sap/core/odata/core/edm/EdmDateTimeOffset.java
+++ odata-core/src/main/java/com/sap/core/odata/core/edm/EdmDateTimeOffset.java
@@ -1,5 +1,9 @@
 package com.sap.core.odata.core.edm;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 import com.sap.core.odata.api.edm.EdmException;
 import com.sap.core.odata.api.edm.EdmFacets;
 import com.sap.core.odata.api.edm.EdmLiteralKind;
@@ -60,12 +64,94 @@ public class EdmDateTimeOffset implements EdmSimpleType {
 
   @Override
   public Object valueOfString(final String value, final EdmLiteralKind literalKind, final EdmFacets facets) {
-    return null;
+    if (value == null)
+      if (facets == null || facets.isNullable() == null || facets.isNullable())
+        return null;
+      else
+        throw new IllegalArgumentException();
+
+    if (literalKind == null)
+      throw new IllegalArgumentException();
+
+    switch (literalKind) {
+    case DEFAULT:
+    case JSON:
+      throw new IllegalArgumentException();
+
+    case URI:
+      throw new IllegalArgumentException();
+
+    default:
+      throw new IllegalArgumentException();
+    }
   }
 
   @Override
   public String valueToString(final Object value, final EdmLiteralKind literalKind, final EdmFacets facets) {
-    return null;
+    if (value == null)
+      if (facets == null)
+        return null;
+      else if (facets.getDefaultValue() == null)
+        if (facets.isNullable() == null || facets.isNullable())
+          return null;
+        else
+          throw new IllegalArgumentException();
+      else
+        return facets.getDefaultValue();
+
+    if (literalKind == null)
+      throw new IllegalArgumentException();
+
+    Calendar dateTimeValue;
+    if (value instanceof Date) {
+      dateTimeValue = Calendar.getInstance();
+      dateTimeValue.clear();
+      dateTimeValue.setTime((Date) value);
+    } else if (value instanceof Calendar) {
+      dateTimeValue = (Calendar) ((Calendar) value).clone();
+    } else if (value instanceof Long) {
+      dateTimeValue = Calendar.getInstance();
+      dateTimeValue.clear();
+      dateTimeValue.setTimeZone(TimeZone.getTimeZone("GMT"));
+      dateTimeValue.setTimeInMillis((Long) value);
+    } else {
+      throw new IllegalArgumentException();
+    }
+
+    final int offset = dateTimeValue.get(Calendar.ZONE_OFFSET) + dateTimeValue.get(Calendar.DST_OFFSET);
+    dateTimeValue.add(Calendar.MILLISECOND, offset); // to counter UTC output below
+    final int offsetInMinutes = offset / 60 / 1000;
+    final int offsetHours = offsetInMinutes / 60;
+    final int offsetMinutes = Math.abs(offsetInMinutes % 60);
+
+    String localTimeString = null;
+    String offsetString = null;
+    if (literalKind != EdmLiteralKind.JSON) {
+      localTimeString = EdmSimpleTypeKind.getSimpleTypeInstance(EdmSimpleTypeKind.DateTime)
+          .valueToString(dateTimeValue, EdmLiteralKind.DEFAULT, facets);
+      if (offset == 0)
+        offsetString = "Z";
+      else
+        offsetString = String.format("%+03d:%02d", offsetHours, offsetMinutes);
+    }
+
+    switch (literalKind) {
+    case DEFAULT:
+      return localTimeString + offsetString;
+
+    case JSON:
+      if (offset == 0)
+        offsetString = "";
+      else
+        offsetString = String.format("%+03d%02d", offsetHours, offsetMinutes);
+      return "\\/Date(" + dateTimeValue.getTimeInMillis() + offsetString + ")\\/";
+
+    case URI:
+      return "datetimeoffset'" + localTimeString + offsetString + "'";
+
+    default:
+      throw new IllegalArgumentException();
+    }
   }
 
   @Override
