a321ff80b33a32776039441851d25ab8ffb9941e
Klaus Straubinger <klaus.straubinger@sap.com>
2012-11-28 09:30:34 +0100

    Edm.Double and Edm.Guid valueToString
    Change-Id: I80fe4d9997b3f608cc0a4a615d80686146ee5595

--- odata-core/src/main/java/com/sap/core/odata/core/edm/EdmGuid.java
+++ odata-core/src/main/java/com/sap/core/odata/core/edm/EdmGuid.java
@@ -12,6 +12,8 @@ import com.sap.core.odata.api.edm.EdmTypeKind;
 
 public class EdmGuid implements EdmSimpleType {
 
+  private static final String PATTERN = "\\p{XDigit}{8}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{4}-\\p{XDigit}{12}";
+
   private static final EdmGuid instance = new EdmGuid();
 
   private EdmGuid() {
@@ -49,22 +51,57 @@ public class EdmGuid implements EdmSimpleType {
 
   @Override
   public boolean validate(final String value, final EdmLiteralKind literalKind, final EdmFacets facets) {
-    try {
-      valueOfString(value, literalKind, facets);
-      return true;
-    } catch (EdmSimpleTypeException e) {
-      return false;
-    }
+    if (value == null)
+      return facets == null || facets.isNullable() == null || facets.isNullable();
+    else if (literalKind == EdmLiteralKind.URI)
+      return value.matches("guid'" + PATTERN + "'");
+    else
+      return value.matches(PATTERN);
   }
 
   @Override
   public UUID valueOfString(final String value, final EdmLiteralKind literalKind, final EdmFacets facets) throws EdmSimpleTypeException {
-    return UUID.fromString(value);
+    if (value == null)
+      if (facets == null || facets.isNullable() == null || facets.isNullable())
+        return null;
+      else
+        throw new EdmSimpleTypeException(EdmSimpleTypeException.LITERAL_NULL_NOT_ALLOWED);
+
+    if (literalKind == null)
+      throw new EdmSimpleTypeException(EdmSimpleTypeException.LITERAL_KIND_MISSING);
+
+    if (validate(value, literalKind, facets))
+      if (literalKind == EdmLiteralKind.URI)
+        return UUID.fromString(value.substring(5, value.length() - 1));
+      else
+        return UUID.fromString(value);
+    else
+      throw new EdmSimpleTypeException(EdmSimpleTypeException.LITERAL_ILLEGAL_CONTENT.addContent(value));
   }
 
   @Override
   public String valueToString(final Object value, final EdmLiteralKind literalKind, final EdmFacets facets) throws EdmSimpleTypeException {
-    return ((UUID) value).toString();
+    if (value == null)
+      if (facets == null)
+        return null;
+      else if (facets.getDefaultValue() == null)
+        if (facets.isNullable() == null || facets.isNullable())
+          return null;
+        else
+          throw new EdmSimpleTypeException(EdmSimpleTypeException.VALUE_NULL_NOT_ALLOWED);
+      else
+        return facets.getDefaultValue();
+
+    if (literalKind == null)
+      throw new EdmSimpleTypeException(EdmSimpleTypeException.LITERAL_KIND_MISSING);
+
+    if (value instanceof UUID)
+      if (literalKind == EdmLiteralKind.URI)
+        return toUriLiteral(((UUID) value).toString());
+      else
+        return ((UUID) value).toString();
+    else
+      throw new EdmSimpleTypeException(EdmSimpleTypeException.VALUE_TYPE_NOT_SUPPORTED.addContent(value.getClass()));
   }
 
   @Override
