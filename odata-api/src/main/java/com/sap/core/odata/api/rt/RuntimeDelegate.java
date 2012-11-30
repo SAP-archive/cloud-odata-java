067634efbfdd79aba405a14e6650befc404447b4
Michael Bolz <michael.bolz@sap.com>
2012-11-30 08:10:42 +0100

    Minor change in 'RuntimeDelegate'
    Change-Id: I2abbd6c926e4723935d150e24292ce5e6669eea1

--- odata-api/src/main/java/com/sap/core/odata/api/rt/RuntimeDelegate.java
+++ odata-api/src/main/java/com/sap/core/odata/api/rt/RuntimeDelegate.java
@@ -27,99 +27,101 @@ public abstract class RuntimeDelegate {
    * Get a RuntimeDelegate Instance through reflection
    * @return {@link RuntimeDelegate} object
    */
-  private static RuntimeDelegate getInstance() {
-    RuntimeDelegate delegate;
+  private static RuntimeDelegateInstance getInstance() {
+    RuntimeDelegateInstance delegate;
 
     try {
       Class<?> clazz = Class.forName(RuntimeDelegate.IMPLEMENTATION);
 
       Object object = clazz.newInstance();
-      delegate = (RuntimeDelegate) object;
+      delegate = (RuntimeDelegateInstance) object;
 
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
     return delegate;
   }
+  
+  public static abstract class RuntimeDelegateInstance {
+    /**
+     * Get a OData response builder
+     * @return {@link ODataResponseBuilder} object
+     */
+    protected abstract ODataResponseBuilder createODataResponseBuilder();
+    
+    /**
+     * Gets an {@link EdmSimpleType} implementation for a given {@link EdmSimpleTypeKind}
+     * @param edmSimpleTypeKind
+     * @return {@link EdmSimpleType}
+     */
+    protected abstract EdmSimpleType getEdmSimpleType(EdmSimpleTypeKind edmSimpleTypeKind);
+    
+    /**
+     * Get a UriParser object
+     * @return {@link UriParser} object
+     */
+    protected abstract UriParser getUriParser(Edm edm);
+    
+    /**
+     * Gets an {@link EdmSimpleType} implementation for a given simple-type name.
+     * @param edmSimpleType  name of the simple type
+     * @return {@link EdmSimpleType}
+     */
+    protected abstract EdmSimpleType getInternalEdmSimpleTypeByString(String edmSimpleType);
+    
+    /**
+     * Gets an implementation of the EDM simple-type facade.
+     * @return {@link EdmSimpleTypeFacade}
+     */
+    protected abstract EdmSimpleTypeFacade getSimpleTypeFacade();
+    
+    /**
+     * Creates an entity data model.
+     * @param provider A {@link EdmProvider} instance
+     * @return {@link Edm} implementation object
+     */
+    protected abstract Edm createEdm(EdmProvider provider);
+    
+    protected abstract FilterParser getFilterParser(Edm edm, EdmType edmType);
+    
+    /**
+     * @param format serializer format
+     * @return a OData serializer
+     * @throws ODataException 
+     */
+    protected abstract ODataSerializer createSerializer(Format format, ODataContext ctx) throws ODataSerializationException;
+  }
 
-  /**
-   * Get a OData response builder
-   * @return {@link ODataResponseBuilder} object
-   */
-  protected abstract ODataResponseBuilder createODataResponseBuilder__();
-
-  /**
-   * Gets an {@link EdmSimpleType} implementation for a given {@link EdmSimpleTypeKind}
-   * @param edmSimpleTypeKind
-   * @return {@link EdmSimpleType}
-   */
-  protected abstract EdmSimpleType getEdmSimpleType__(EdmSimpleTypeKind edmSimpleTypeKind);
-
-  /**
-   * Get a UriParser object
-   * @return {@link UriParser} object
-   */
-  protected abstract UriParser getUriParser__(Edm edm);
-
-  /**
-   * Gets an {@link EdmSimpleType} implementation for a given simple-type name.
-   * @param edmSimpleType  name of the simple type
-   * @return {@link EdmSimpleType}
-   */
-  protected abstract EdmSimpleType getInternalEdmSimpleTypeByString__(String edmSimpleType);
-
-  /**
-   * Gets an implementation of the EDM simple-type facade.
-   * @return {@link EdmSimpleTypeFacade}
-   */
-  protected abstract EdmSimpleTypeFacade getSimpleTypeFacade__();
-
-  /**
-   * Creates an entity data model.
-   * @param provider A {@link EdmProvider} instance
-   * @return {@link Edm} implementation object
-   */
-  protected abstract Edm createEdm__(EdmProvider provider);
-
-  protected abstract FilterParser getFilterParser__(Edm edm, EdmType edmType);
-
-  /**
-   * @param format serializer format
-   * @return a OData serializer
-   * @throws ODataException 
-   */
-  protected abstract ODataSerializer createSerializer__(Format format, ODataContext ctx) throws ODataSerializationException;
 
   public static ODataSerializer createSerializer(Format atom, ODataContext ctx) throws ODataSerializationException {
-    return RuntimeDelegate.getInstance().createSerializer__(atom, ctx);
+    return RuntimeDelegate.getInstance().createSerializer(atom, ctx);
   }
 
   public static EdmSimpleType getEdmSimpleType(EdmSimpleTypeKind edmSimpleType) {
-    return RuntimeDelegate.getInstance().getEdmSimpleType__(edmSimpleType);
+    return RuntimeDelegate.getInstance().getEdmSimpleType(edmSimpleType);
   }
 
   public static EdmSimpleTypeFacade getSimpleTypeFacade() {
-    return RuntimeDelegate.getInstance().getSimpleTypeFacade__();
+    return RuntimeDelegate.getInstance().getSimpleTypeFacade();
   }
 
   public static ODataResponseBuilder createODataResponseBuilder() {
-    return RuntimeDelegate.getInstance().createODataResponseBuilder__();
+    return RuntimeDelegate.getInstance().createODataResponseBuilder();
   }
 
   public static Edm createEdm(EdmProvider provider) {
-    return RuntimeDelegate.getInstance().createEdm__(provider);
+    return RuntimeDelegate.getInstance().createEdm(provider);
   }
 
   public static UriParser getUriParser(Edm edm) {
-    return RuntimeDelegate.getInstance().getUriParser__(edm);
+    return RuntimeDelegate.getInstance().getUriParser(edm);
   }
 
   public static EdmSimpleType getInternalEdmSimpleTypeByString(String edmSimpleType) {
-    return RuntimeDelegate.getInstance().getInternalEdmSimpleTypeByString__(edmSimpleType);
+    return RuntimeDelegate.getInstance().getInternalEdmSimpleTypeByString(edmSimpleType);
   }
 
   public static FilterParser getFilterParser(Edm edm, EdmType edmType) {
-    return RuntimeDelegate.getInstance().getFilterParser__(edm, edmType);
+    return RuntimeDelegate.getInstance().getFilterParser(edm, edmType);
   }
-
 }
