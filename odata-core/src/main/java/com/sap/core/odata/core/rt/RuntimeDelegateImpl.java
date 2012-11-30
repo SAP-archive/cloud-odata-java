431a0c1ea69e96b717845acbdce99b43a309097b
Michael Bolz <michael.bolz@sap.com>
2012-11-30 08:10:42 +0100

    Minor change in 'RuntimeDelegate'
    Change-Id: I2abbd6c926e4723935d150e24292ce5e6669eea1

--- odata-core/src/main/java/com/sap/core/odata/core/rt/RuntimeDelegateImpl.java
+++ odata-core/src/main/java/com/sap/core/odata/core/rt/RuntimeDelegateImpl.java
@@ -9,7 +9,7 @@ import com.sap.core.odata.api.edm.provider.EdmProvider;
 import com.sap.core.odata.api.enums.Format;
 import com.sap.core.odata.api.processor.ODataContext;
 import com.sap.core.odata.api.processor.ODataResponse.ODataResponseBuilder;
-import com.sap.core.odata.api.rt.RuntimeDelegate;
+import com.sap.core.odata.api.rt.RuntimeDelegate.RuntimeDelegateInstance;
 import com.sap.core.odata.api.serialization.ODataSerializationException;
 import com.sap.core.odata.api.serialization.ODataSerializer;
 import com.sap.core.odata.api.uri.UriParser;
@@ -21,44 +21,44 @@ import com.sap.core.odata.core.serializer.ODataSerializerFactory;
 import com.sap.core.odata.core.uri.UriParserImpl;
 import com.sap.core.odata.core.uri.expression.FilterParserImpl;
 
-public class RuntimeDelegateImpl extends RuntimeDelegate {
+public class RuntimeDelegateImpl extends RuntimeDelegateInstance {
 
   @Override
-  protected ODataResponseBuilder createODataResponseBuilder__() {
+  protected ODataResponseBuilder createODataResponseBuilder() {
     return new ODataResponseBuilderImpl();
   }
 
-  protected EdmSimpleType getEdmSimpleType__(EdmSimpleTypeKind edmSimpleType) {
+  protected EdmSimpleType getEdmSimpleType(EdmSimpleTypeKind edmSimpleType) {
     return EdmSimpleTypeFacadeImpl.getEdmSimpleType(edmSimpleType);
   }
 
   @Override
-  protected UriParser getUriParser__(Edm edm) {
+  protected UriParser getUriParser(Edm edm) {
     return new UriParserImpl(edm);
   }
 
   @Override
-  protected EdmSimpleType getInternalEdmSimpleTypeByString__(String edmSimpleType) {
+  protected EdmSimpleType getInternalEdmSimpleTypeByString(String edmSimpleType) {
     return EdmSimpleTypeFacadeImpl.getInternalEdmSimpleTypeByString(edmSimpleType);
   }
 
   @Override
-  protected EdmSimpleTypeFacade getSimpleTypeFacade__() {
+  protected EdmSimpleTypeFacade getSimpleTypeFacade() {
     return new EdmSimpleTypeFacadeImpl();
   }
 
   @Override
-  protected Edm createEdm__(EdmProvider provider) {
+  protected Edm createEdm(EdmProvider provider) {
     return new EdmImplProv(provider);
   }
 
   @Override
-  protected ODataSerializer createSerializer__(Format format, ODataContext ctx) throws ODataSerializationException {
+  protected ODataSerializer createSerializer(Format format, ODataContext ctx) throws ODataSerializationException {
     return ODataSerializerFactory.create(format, ctx);
   }
 
   @Override
-  protected FilterParser getFilterParser__(Edm edm, EdmType edmType) {
+  protected FilterParser getFilterParser(Edm edm, EdmType edmType) {
     return new FilterParserImpl(edm, edmType);
   }
 
