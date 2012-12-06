b89e0d87c3a7eda19bb6cc2d0da4ded2a2ed4638
Christian Amend <christian.amend@sap.com>
2012-12-06 13:46:20 +0100

    Enhance technical provider further
    added a entity type which contains String properties and
    some facets
    
    Change-Id: I7ab5d0d2f9e631749cd6204b65b73de7b05e34a4

--- odata-testutils/src/main/java/com/sap/core/odata/testutils/mocks/TechnicalScenarioEdmProvider.java
+++ odata-testutils/src/main/java/com/sap/core/odata/testutils/mocks/TechnicalScenarioEdmProvider.java
@@ -40,7 +40,8 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
   private static final FullQualifiedName ET_KEY_IS_INTEGER = new FullQualifiedName(NAMESPACE_1, "EtKeyTypeInteger");
   private static final FullQualifiedName ET_COMPLEX_KEY = new FullQualifiedName(NAMESPACE_1, "EtComplexKey");
   private static final FullQualifiedName ET_ALL_TYPES = new FullQualifiedName(NAMESPACE_1, "EtAllTypes");
-  
+  private static final FullQualifiedName ET_STRING_FACETS = new FullQualifiedName(NAMESPACE_1, "EtStringFacets");
+
   private static final FullQualifiedName CT_ADDRESS = new FullQualifiedName(NAMESPACE_1, "CtAdress");
   private static final FullQualifiedName CT_ALL_TYPES = new FullQualifiedName(NAMESPACE_1, "CtAllTypes");
 
@@ -49,11 +50,12 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
   private static final String ROLE_2 = "Role2";
 
   private static final String ENTITY_CONTAINER_1 = "Container1";
-  
+
   private static final String ES_KEY_IS_STRING = "KeyTypeString";
   private static final String ES_KEY_IS_INTEGER = "KeyTypeInteger";
   private static final String ES_COMPLEX_KEY = "ComplexKey";
   private static final String ES_ALL_TYPES = "AllTypes";
+  private static final String ES_STRING_FACETS = "StringFacets";
 
   @Override
   public Collection<Schema> getSchemas() throws ODataMessageException {
@@ -68,7 +70,7 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
     entityTypes.add(getEntityType(ET_COMPLEX_KEY));
     entityTypes.add(getEntityType(ET_ALL_TYPES));
     schema.setEntityTypes(entityTypes);
-    
+
     List<ComplexType> complexTypes = new ArrayList<ComplexType>();
     complexTypes.add(getComplexType(CT_ALL_TYPES));
     schema.setComplexTypes(complexTypes);
@@ -82,6 +84,7 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
     entitySets.add(getEntitySet(ENTITY_CONTAINER_1, ES_KEY_IS_INTEGER));
     entitySets.add(getEntitySet(ENTITY_CONTAINER_1, ES_COMPLEX_KEY));
     entitySets.add(getEntitySet(ENTITY_CONTAINER_1, ES_ALL_TYPES));
+    entitySets.add(getEntitySet(ENTITY_CONTAINER_1, ES_STRING_FACETS));
     entityContainer.setEntitySets(entitySets);
 
     entityContainers.add(entityContainer);
@@ -128,7 +131,7 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
         properties.add(new SimpleProperty().setName("KeyInteger")
             .setType(EdmSimpleTypeKind.String)
             .setFacets(new Facets().setNullable(false)));
-        return new EntityType().setName(ET_COMPLEX_KEY.getName()).setKey(createKey("KeyInteger", "KeyString"));
+        return new EntityType().setName(ET_COMPLEX_KEY.getName()).setProperties(properties).setKey(createKey("KeyInteger", "KeyString"));
       }
       else if (ET_ALL_TYPES.getName().equals(edmFQName.getName()))
       {
@@ -149,7 +152,34 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
         properties.add(new SimpleProperty().setName("String").setType(EdmSimpleTypeKind.String));
         properties.add(new SimpleProperty().setName("Time").setType(EdmSimpleTypeKind.Time));
         properties.add(new ComplexProperty().setName("ComplexType").setType(CT_ALL_TYPES));
-        return new EntityType().setName(ET_ALL_TYPES.getName());
+        return new EntityType().setName(ET_ALL_TYPES.getName()).setProperties(properties);
+      }
+      else if (ET_STRING_FACETS.getName().equals(edmFQName.getName()))
+      {
+        Collection<Property> properties = new ArrayList<Property>();
+        //TODO: What here?
+//        properties.add(new SimpleProperty().setName("StringCollation").setType(EdmSimpleTypeKind.String)
+//            .setFacets(new Facets().setCollation(collation)));
+        
+        properties.add(new SimpleProperty().setName("StringDefaultValue").setType(EdmSimpleTypeKind.String)
+            .setFacets(new Facets().setDefaultValue("defaultValue")));
+        
+        properties.add(new SimpleProperty().setName("StringFixedLength").setType(EdmSimpleTypeKind.String)
+            .setFacets(new Facets().setFixedLength(true)));
+        
+        properties.add(new SimpleProperty().setName("StringMaxLength").setType(EdmSimpleTypeKind.String)
+            .setFacets(new Facets().setMaxLength(15)));
+        
+        properties.add(new SimpleProperty().setName("StringLength").setType(EdmSimpleTypeKind.String)
+            .setFacets(new Facets().setMaxLength(15).setFixedLength(true)));
+        
+        properties.add(new SimpleProperty().setName("StringNullable").setType(EdmSimpleTypeKind.String)
+            .setFacets(new Facets().setNullable(false)));
+         
+        properties.add(new SimpleProperty().setName("StringUnicode").setType(EdmSimpleTypeKind.String)
+            .setFacets(new Facets().setUnicode(false)));
+        
+        return new EntityType().setName(ET_STRING_FACETS.getName()).setProperties(properties);
       }
     }
 
@@ -157,7 +187,7 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
   }
 
   @Override
-  public ComplexType getComplexType(final FullQualifiedName edmFQName) throws ODataMessageException 
+  public ComplexType getComplexType(final FullQualifiedName edmFQName) throws ODataMessageException
   {
     if (NAMESPACE_1.equals(edmFQName.getNamespace()))
       if (CT_ADDRESS.getName().equals(edmFQName.getName())) {
@@ -185,21 +215,21 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
         properties.add(new SimpleProperty().setName("Time").setType(EdmSimpleTypeKind.Time));
         properties.add(new ComplexProperty().setName("Adress").setType(CT_ADDRESS));
         return new ComplexType().setName(CT_ALL_TYPES.getName()).setAbstract(false).setProperties(properties);
-      } 
+      }
 
     return null;
   }
 
   @Override
   public Association getAssociation(final FullQualifiedName edmFQName) throws ODataMessageException {
-    if(NAMESPACE_1.equals(edmFQName.getNamespace())){
-      if(ASSOCIATION_ET1_ET2.getName().equals(edmFQName.getName())){
+    if (NAMESPACE_1.equals(edmFQName.getNamespace())) {
+      if (ASSOCIATION_ET1_ET2.getName().equals(edmFQName.getName())) {
         AssociationEnd end1 = new AssociationEnd().setMultiplicity(EdmMultiplicity.ONE).setRole(ROLE_1).setType(ET_KEY_IS_STRING);
-        AssociationEnd end2 = new AssociationEnd().setMultiplicity(EdmMultiplicity.ONE).setRole(ROLE_2).setType(ET_KEY_IS_INTEGER);        
+        AssociationEnd end2 = new AssociationEnd().setMultiplicity(EdmMultiplicity.ONE).setRole(ROLE_2).setType(ET_KEY_IS_INTEGER);
         return new Association().setName("Association").setEnd1(end1).setEnd2(end2);
       }
     }
-    
+
     return null;
   }
 
@@ -213,7 +243,7 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
 
   @Override
   public EntitySet getEntitySet(final String entityContainer, final String name) throws ODataMessageException {
-    if (ENTITY_CONTAINER_1.equals(entityContainer)) 
+    if (ENTITY_CONTAINER_1.equals(entityContainer))
     {
       if (ES_KEY_IS_STRING.equals(name))
         return new EntitySet().setName(name).setEntityType(ET_KEY_IS_STRING);
@@ -223,6 +253,8 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
         return new EntitySet().setName(name).setEntityType(ET_COMPLEX_KEY);
       else if (ES_ALL_TYPES.equals(name))
         return new EntitySet().setName(name).setEntityType(ET_ALL_TYPES);
+      else if (ES_STRING_FACETS.equals(name))
+        return new EntitySet().setName(name).setEntityType(ET_STRING_FACETS);
     }
 
     return null;
@@ -235,11 +267,11 @@ public class TechnicalScenarioEdmProvider extends EdmProviderDefault {
 
   @Override
   public AssociationSet getAssociationSet(final String entityContainer, final FullQualifiedName association, final String sourceEntitySetName, final String sourceEntitySetRole) throws ODataMessageException {
-    if(ENTITY_CONTAINER_1.equals(entityContainer)){
-      if(ASSOCIATION_ET1_ET2.equals(association)){
+    if (ENTITY_CONTAINER_1.equals(entityContainer)) {
+      if (ASSOCIATION_ET1_ET2.equals(association)) {
         AssociationSetEnd end1 = new AssociationSetEnd().setRole(ROLE_1).setEntitySet(ES_KEY_IS_STRING);
         AssociationSetEnd end2 = new AssociationSetEnd().setRole(ROLE_2).setEntitySet(ES_KEY_IS_INTEGER);
-        
+
         return new AssociationSet().setName("AssociationSet").setEnd1(end1).setEnd2(end2);
       }
     }
