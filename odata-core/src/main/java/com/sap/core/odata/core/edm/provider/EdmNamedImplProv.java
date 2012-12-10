223f5f71ded0b7bfd1d4e0f2d3a5283c7e218553
Christian Amend <christian.amend@sap.com>
2012-12-10 16:44:05 +0100

    Implement validate name
    Change-Id: I428bdc78c505bc42a666c44dc3c7f8b415081211

--- odata-core/src/main/java/com/sap/core/odata/core/edm/provider/EdmNamedImplProv.java
+++ odata-core/src/main/java/com/sap/core/odata/core/edm/provider/EdmNamedImplProv.java
@@ -1,10 +1,21 @@
 package com.sap.core.odata.core.edm.provider;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.sap.core.odata.api.edm.EdmException;
 import com.sap.core.odata.api.edm.EdmNamed;
 
 public abstract class EdmNamedImplProv implements EdmNamed {
 
+  private static final Pattern pattern = Pattern
+      .compile("^[:A-Z_a-z\\u00C0\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02ff\\u0370-\\u037d"
+          + "\\u037f-\\u1fff\\u200c\\u200d\\u2070-\\u218f\\u2c00-\\u2fef\\u3001-\\ud7ff"
+          + "\\uf900-\\ufdcf\\ufdf0-\\ufffd\\x10000-\\xEFFFF]"
+          + "[:A-Z_a-z\\u00C0\\u00D6\\u00D8-\\u00F6"
+          + "\\u00F8-\\u02ff\\u0370-\\u037d\\u037f-\\u1fff\\u200c\\u200d\\u2070-\\u218f"
+          + "\\u2c00-\\u2fef\\u3001-\\udfff\\uf900-\\ufdcf\\ufdf0-\\ufffd\\-\\.0-9"
+          + "\\u00b7\\u0300-\\u036f\\u203f-\\u2040]*\\Z");
   protected EdmImplProv edm;
   private String name;
 
@@ -20,6 +31,9 @@ public abstract class EdmNamedImplProv implements EdmNamed {
   }
 
   private void validateName(String name) throws EdmException {
-    //TODO
+    Matcher matcher = pattern.matcher(name);
+    if (!matcher.matches()) {
+      throw new EdmException(EdmException.COMMON);
+    }
   }
 }
