6b009600388fa8eaebf2229022a6f7b8585b1264
Michael Bolz <michael.bolz@sap.com>
2012-12-11 14:56:09 +0100

    Added CSB to performance test
    Change-Id: Ifa43c05a77aa54d4f9f10c8cf224f04f60c819db

--- odata-core/src/test/java/com/sap/core/odata/core/ep/PerformanceTest.java
+++ odata-core/src/test/java/com/sap/core/odata/core/ep/PerformanceTest.java
@@ -2,8 +2,11 @@ package com.sap.core.odata.core.ep;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.stream.XMLOutputFactory;
@@ -20,25 +23,37 @@ import com.sap.core.odata.api.edm.Edm;
 import com.sap.core.odata.api.edm.EdmEntitySet;
 import com.sap.core.odata.api.exception.ODataException;
 import com.sap.core.odata.core.ep.aggregator.EntityInfoAggregator;
+import com.sap.core.odata.core.ep.util.CircleStreamBuffer;
+import com.sap.core.odata.testutils.helper.StringHelper;
 import com.sap.core.odata.testutils.mocks.MockFacade;
 
 public class PerformanceTest extends AbstractProviderTest {
 
-  private static final long TIMES = 500L;
+  private static final long TIMES = 100L;
 
   private AtomEntryEntityProvider provider;
   private EdmEntitySet edmEntitySet;
-  private ByteArrayOutputStream outStream = null;
+  private OutputStream outStream = null;
+  private CircleStreamBuffer csb;
   private XMLStreamWriter writer;
 
+  private boolean useCsb = true;
+  
   @Before
   public void before() throws Exception {
     super.before();
+    System.gc();
 
     provider = new AtomEntryEntityProvider(createContextMock());
     edmEntitySet = MockFacade.getMockEdm().getDefaultEntityContainer().getEntitySet("Rooms");
 
-    outStream = new ByteArrayOutputStream();
+    if(useCsb) {
+      csb = new CircleStreamBuffer();
+      outStream = csb.getOutputStream();
+    } else {
+      outStream = new ByteArrayOutputStream();
+    }
+
     writer = XMLOutputFactory.newInstance().createXMLStreamWriter(outStream, "utf-8");
 
     writer.writeStartElement("junit");
@@ -54,13 +69,22 @@ public class PerformanceTest extends AbstractProviderTest {
     writer.flush();
     outStream.flush();
 
-    assertNotNull(outStream.toByteArray());
+    if(useCsb) {
+      String content = StringHelper.inputStreamToString(csb.getInputStream());
+      assertNotNull(content);
+    } else {
+      InputStream in = new ByteArrayInputStream(((ByteArrayOutputStream)outStream).toByteArray());
+      String content = StringHelper.inputStreamToString(in);
+      assertNotNull(content);
+//      assertNotNull(((ByteArrayOutputStream)outStream).toByteArray());      
+    }
 
     try {
 
     } finally {
       outStream.close();
     }
+    //
   }
 
   @Test
@@ -75,7 +99,23 @@ public class PerformanceTest extends AbstractProviderTest {
   }
 
   @Test
+  public void readAtomEntryCsb() throws Exception {
+    useCsb = true;
+    before();
+    assertNotNull(csb);
+    
+    long t = startTimer();
+
+    for (int i = 0; i < TIMES; i++) {
+      EntityInfoAggregator eia = EntityInfoAggregator.create(edmEntitySet);
+      provider.append(writer, eia, roomData, false, null);
+    }
+    stopTimer(t, "readAtomEntry");
+  }
+
+  @Test
   public void readAtomEntryOptimized() throws IOException, XpathException, SAXException, XMLStreamException, FactoryConfigurationError, ODataException {
+    printMemoryUsage("readAtomEntryOptimized -> Start");
     long t = startTimer();
 
     EntityInfoAggregator eia = EntityInfoAggregator.create(edmEntitySet);
@@ -83,8 +123,38 @@ public class PerformanceTest extends AbstractProviderTest {
       provider.append(writer, eia, roomData, false, null);
     }
     stopTimer(t, "readAtomEntryOptimized");
+    printMemoryUsage("readAtomEntryOptimized -> Finish");
   }
 
+  @Test
+  public void readAtomEntryOptimizedCsb() throws Exception {
+    useCsb = true;
+    before();
+    assertNotNull(csb);
+
+    printMemoryUsage("readAtomEntryOptimizedCsb -> Start");
+    long t = startTimer();
+
+    EntityInfoAggregator eia = EntityInfoAggregator.create(edmEntitySet);
+    for (int i = 0; i < TIMES; i++) {
+      provider.append(writer, eia, roomData, false, null);
+    }
+    stopTimer(t, "readAtomEntryOptimizedCsb");
+    printMemoryUsage("readAtomEntryOptimizedCsb -> Finish");
+  }
+
+  private void printMemoryUsage(String testName) {
+    log.debug("-------------------------------------------");
+    log.debug("Memory usage for test: {}", testName);
+    long freeMemory = Runtime.getRuntime().freeMemory();
+    log.debug("Free Memory (kbytes): {}", freeMemory/1024);
+    long maxMemory = Runtime.getRuntime().maxMemory();
+    log.debug("Maximum memory (kbytes): {}", (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory/1024));
+    long memory = Runtime.getRuntime().totalMemory();
+    log.debug("Total Memory (kbytes): {}", memory/1024);
+    log.debug("-------------------------------------------");
+  }
+  
   private void stopTimer(long t, String msg) {
     t = (System.nanoTime() - t) / TIMES;
 
