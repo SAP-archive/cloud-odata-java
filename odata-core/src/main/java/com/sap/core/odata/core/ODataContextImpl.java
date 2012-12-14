511246709768139f3880b0bb0c949378a16707ca
Stephan Klevenz <stephan.klevenz@sap.com>
2012-12-14 14:50:40 +0100

    re-factor ODataContext: remove unused feature
    Change-Id: I1230d63c06b63e17641072013011b1546c71d3c5

--- odata-core/src/main/java/com/sap/core/odata/core/ODataContextImpl.java
+++ odata-core/src/main/java/com/sap/core/odata/core/ODataContextImpl.java
@@ -1,7 +1,5 @@
 package com.sap.core.odata.core;
 
-import java.util.HashMap;
-
 import com.sap.core.odata.api.exception.ODataException;
 import com.sap.core.odata.api.processor.ODataContext;
 import com.sap.core.odata.api.processor.ODataUriInfo;
@@ -9,24 +7,12 @@ import com.sap.core.odata.api.service.ODataService;
 
 public class ODataContextImpl implements ODataContext {
 
-  private HashMap<String, Object> contextObjects = new HashMap<String, Object>();
-
   private ODataService service;
 
   private ODataUriInfo uriInfo;
 
-  public void putContextObject(String key, Object obj) throws ODataException {
-    this.contextObjects.put(key, obj);
-  }
-
   public void setService(ODataService service) {
     this.service = service;
-  }
-
-  @SuppressWarnings("unchecked")
-  @Override
-  public <T> T getObject(String key) throws ODataException {
-    return (T) this.contextObjects.get(key);
   }
 
   @Override
