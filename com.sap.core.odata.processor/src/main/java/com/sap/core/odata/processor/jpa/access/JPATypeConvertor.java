aee9b83d7b62320a102defcd42dabed38bca0234
Santosh Kumar <santosh.kumar04@sap.com>
2012-12-21 16:33:59 +0530

    Changes for type converter as per backlog - JavaPersistencyApi: Advanced concepts in Metadata Enablement.
    Change-Id: Ie1270625e93b50fb64afd61ca4a60ddac451d7c6
    Signed-off-by: Santosh Kumar <santosh.kumar04@sap.com>

--- com.sap.core.odata.processor/src/main/java/com/sap/core/odata/processor/jpa/access/JPATypeConvertor.java
+++ com.sap.core.odata.processor/src/main/java/com/sap/core/odata/processor/jpa/access/JPATypeConvertor.java
@@ -1,5 +1,10 @@
 package com.sap.core.odata.processor.jpa.access;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
 import com.sap.core.odata.api.edm.EdmSimpleTypeKind;
 
 public class JPATypeConvertor {
@@ -8,12 +13,32 @@ public class JPATypeConvertor {
 		
 		if (jpaType.equals(String.class))
 			return EdmSimpleTypeKind.String;
-		else if (jpaType.equals(Long.class))
+		else if (jpaType.equals(Long.class) || jpaType.equals(long.class))
 			return EdmSimpleTypeKind.Int64;
-		else if (jpaType.equals(Short.class))
+		else if (jpaType.equals(Short.class) || jpaType.equals(short.class))
 			return EdmSimpleTypeKind.Int16;
-		else if (jpaType.equals(Integer.class))
+		else if (jpaType.equals(Integer.class) || jpaType.equals(int.class))
 			return EdmSimpleTypeKind.Int32;
+		else if (jpaType.equals(Double.class) || jpaType.equals(double.class))
+			return EdmSimpleTypeKind.Double;
+		else if (jpaType.equals(Float.class) ||  jpaType.equals(float.class))
+			return EdmSimpleTypeKind.Single;
+		else if (jpaType.equals(BigDecimal.class))
+			return EdmSimpleTypeKind.Decimal;
+		else if (jpaType.equals(byte[].class))
+			return EdmSimpleTypeKind.Binary;
+		else if (jpaType.equals(Byte.class))
+			return EdmSimpleTypeKind.Byte;
+		else if (jpaType.equals(Byte[].class))
+			return EdmSimpleTypeKind.Binary;
+		else if (jpaType.equals(Boolean.class))
+			return EdmSimpleTypeKind.Boolean;
+		else if (jpaType.equals(Date.class))
+			return EdmSimpleTypeKind.DateTime;
+		else if (jpaType.equals(Calendar.class))
+			return EdmSimpleTypeKind.DateTime;
+		else if (jpaType.equals(UUID.class))
+			return EdmSimpleTypeKind.Guid;
 		
 		return null;
 	}
