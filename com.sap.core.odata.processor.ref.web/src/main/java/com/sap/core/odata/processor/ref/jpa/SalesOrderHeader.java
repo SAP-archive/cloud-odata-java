6fc6c7e23e47f920331b76c59147e8020ab75039
Anirban Roy <anirban.roy@sap.com>
2012-12-24 16:24:13 +0530

    Change in JPA model Change-Id: I5b3f40602b67084c27ef75372c7b8bbe3be15979 Signed-off-by: Anirban Roy <anirban.roy@sap.com>

--- com.sap.core.odata.processor.ref.web/src/main/java/com/sap/core/odata/processor/ref/jpa/SalesOrderHeader.java
+++ com.sap.core.odata.processor.ref.web/src/main/java/com/sap/core/odata/processor/ref/jpa/SalesOrderHeader.java
@@ -1,5 +1,7 @@
 package com.sap.core.odata.processor.ref.jpa;
 
+import java.util.Calendar;
+
 import javax.persistence.*;
 
 @Entity
@@ -9,6 +11,46 @@ public class SalesOrderHeader {
 
 	@Id
 	private long id;
+	@Column(name = "NAME")
+	  private String name;
+	@Column(name = "DESCRIPTION")
+	  private String description;
+	@Column(name = "PRICE")
+		private int price;
+	@Temporal(TemporalType.TIMESTAMP)
+		private Calendar deliveryDate;
+
+	public Calendar getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(Calendar deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
 	public long getId() {
 		return id;
