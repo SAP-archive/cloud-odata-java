9e31180b10e5c20a4970369e62440de6811dd8da
Apoorva Kumar <apoorva.kumar@sap.com>
2013-01-07 15:17:52 +0530

    Data Generator modified for duplicate content and additional fields of SalesOrderItem
    Change-Id: Id5e47f2049a3af57f9b6d5813c6386f5e4bb8c37
    Signed-off-by: Apoorva Kumar <apoorva.kumar@sap.com>

--- com.sap.core.odata.processor.ref.web/src/main/java/com/sap/core/odata/processor/ref/util/DataGenerator.java
+++ com.sap.core.odata.processor.ref.web/src/main/java/com/sap/core/odata/processor/ref/util/DataGenerator.java
@@ -1,4 +1,4 @@
-package com.sap.core.odata.processor.ref;
+package com.sap.core.odata.processor.ref.util;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -20,6 +20,7 @@ public class DataGenerator {
 
 	public void generate() {
 		this.entityManager.getTransaction().begin();
+		int count = 0;
 		for (int i = 0; i < DataGenerator.MAX_SALESORDER; i++) {
 			Address ba = new Address((short) i, "Street_" + i,
 					"City_" + i, "Country_" + i);
@@ -27,11 +28,16 @@ public class DataGenerator {
 					ba, "Cur_Code_" + i, (double) i, ((i % 2) == 0) ? true
 							: false);
 			for (int j = 0; j < DataGenerator.MAX_SALESORDERITEM_PER_SALESORDER; j++) {
-				SalesOrderItem salesOrderItem = new SalesOrderItem("SalesOrderItem_" + j);
+				SalesOrderItem salesOrderItem = new SalesOrderItem("SalesOrderItem_" + j,j,(double) ((j*200)/4));
 				salesOrder.getSalesOrderItem().add(salesOrderItem);
 				this.entityManager.persist(salesOrderItem); 
 			}
 			this.entityManager.persist(salesOrder);
+			
+			count++;
+			
+			if((i==7)&&(count==8))
+			{ i--;}
 		}
 		this.entityManager.getTransaction().commit();
 	}
