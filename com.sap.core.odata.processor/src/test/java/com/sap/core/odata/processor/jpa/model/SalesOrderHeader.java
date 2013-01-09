b038e0c6bf8216592abe7da6694b422869e6e73e
Anirban Roy <anirban.roy@sap.com>
2013-01-09 12:35:02 +0530

    EDMBuilderV2 Junit added Change-Id: I72f996e4b1959b18494e9a09023c75141405debf Signed-off-by: Anirban Roy <anirban.roy@sap.com>

---
+++ com.sap.core.odata.processor/src/test/java/com/sap/core/odata/processor/jpa/model/SalesOrderHeader.java
@@ -0,0 +1,114 @@
+package com.sap.core.odata.processor.jpa.model;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "T_SALESORDERHEADER")
+@NamedQuery(name = "AllSOHeader", query = "select so from SalesOrderHeader so")
+public class SalesOrderHeader {
+
+	public SalesOrderHeader() {
+		this.creationDate = new Date(System.currentTimeMillis());
+		//No arguement constructor
+	}
+		
+
+	public SalesOrderHeader(int buyerId, String buyerName,
+			String currencyCode, double netAmount,
+			boolean deliveryStatus) {
+		
+		this();
+		this.buyerId = buyerId;
+		this.buyerName = buyerName;
+		this.currencyCode = currencyCode;
+		this.netAmount = netAmount;
+		this.deliveryStatus = deliveryStatus;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "SO_ID")
+	private long soId;
+
+	@Temporal(TemporalType.DATE)
+	private Date creationDate;
+
+	@Column(name = "BUYER_ID")
+	private int buyerId;
+
+	@Column(name = "BUYER_NAME")
+	private String buyerName;
+
+	
+
+	@Column(name = "CURRENCY_CODE",length = 10)
+	private String currencyCode;
+
+	@Column(name = "NET_AMOUNT",precision = 5)
+	private double netAmount;
+
+	@Column(name = "DELIVERY_STATUS")
+	private boolean deliveryStatus;
+
+	
+
+	public long getSoId() {
+		return soId;
+	}
+
+	public void setSoId(long soId) {
+		this.soId = soId;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public int getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(int buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	public String getBuyerName() {
+		return buyerName;
+	}
+
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+
+	
+	public String getCurrencyCode() {
+		return currencyCode;
+	}
+
+	public void setCurrencyCode(String currencyCode) {
+		this.currencyCode = currencyCode;
+	}
+
+	public double getNetAmount() {
+		return netAmount;
+	}
+
+	public void setNetAmount(double netAmount) {
+		this.netAmount = netAmount;
+	}
+
+	public boolean getDeliveryStatus() {
+		return deliveryStatus;
+	}
+
+	public void setDeliveryStatus(boolean deliveryStatus) {
+		this.deliveryStatus = deliveryStatus;
+	}
+
+	
+}
