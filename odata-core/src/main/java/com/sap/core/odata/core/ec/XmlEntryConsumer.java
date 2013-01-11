b3ea6a43722aa3402b143995dfcc6ae6468ef39e
Michael Bolz <michael.bolz@sap.com>
2013-01-11 14:30:38 +0100

    Implementation of XmlEntryConsumer started
    Change-Id: I09aa7b07e25fd096d0274fcade02ad1fbcca1b30

--- odata-core/src/main/java/com/sap/core/odata/core/ec/XmlEntryConsumer.java
+++ odata-core/src/main/java/com/sap/core/odata/core/ec/XmlEntryConsumer.java
@@ -14,43 +14,186 @@ public class XmlEntryConsumer {
 
   public Map<String, Object> readEntry(XMLStreamReader reader, EdmEntitySet entitySet) throws EntityConsumerException {
     try {
-      Map<String, Object> resultMap = internalRead(reader, entitySet);
+//      Map<String, Object> resultMap = readProperties(reader, entitySet);
+      Map<String, Object> resultMap = readNextTag(reader, entitySet);
       return resultMap;
     } catch (Exception e) {
       throw new EntityConsumerException(EntityConsumerException.COMMON, e);
     }
   }
+  
+  private Map<String, Object> readNextTag(XMLStreamReader reader, EdmEntitySet entitySet) throws Exception {
+    Map<String, Object> result = new HashMap<String, Object>();
+    
+    int eventType;
+    while((eventType = reader.next()) != XMLStreamReader.END_DOCUMENT) {
+      if(eventType == XMLStreamReader.START_ELEMENT) {
+        String tagName = reader.getLocalName();
+        
+        Object value = handleStartedTag(reader, tagName, entitySet);
+        result.put(tagName, value);
+      }
+    }
+    
+    return result;
+  }
 
-  private Map<String, Object> internalRead(XMLStreamReader reader, EdmEntitySet entitySet) throws Exception {
+  private Object handleStartedTag(XMLStreamReader reader, String tagName, EdmEntitySet entitySet) throws Exception {
+    if("id".equals(tagName)) {
+      return readId(reader);
+    } else if("title".equals(tagName)) {
+      return readTitle(reader);
+    } else if("updated".equals(tagName)) {
+      return readUpdated(reader);
+    } else if("category".equals(tagName)) {
+      return readCategory(reader);
+    } else if("link".equals(tagName)) {
+      return readLink(reader);
+    } else if("content".equals(tagName)) {
+      return readContent(reader, entitySet);
+    } else if("properties".equals(tagName)) {
+      return readProperties(reader, entitySet);
+    }
+    
+    return null;
+  }
+
+  private Object readLink(XMLStreamReader reader) throws EntityConsumerException, XMLStreamException {
+    validateStartPosition(reader, "link");
+    Map<String, String> attributes = readAttributes(reader);
+    readAndValidateEndPosition(reader, "link");
+    
+    return attributes;
+  }
+
+  private void validateStartPosition(XMLStreamReader reader, String tagName) throws EntityConsumerException {
+    validatePosition(reader, tagName, XMLStreamReader.START_ELEMENT);
+  }
+
+  private void validateEndPosition(XMLStreamReader reader, String tagName) throws EntityConsumerException {
+    validatePosition(reader, tagName, XMLStreamReader.END_ELEMENT);
+  }
+
+  private void readAndValidateEndPosition(XMLStreamReader reader, String tagName) throws EntityConsumerException, XMLStreamException {
+    readAndValidatePosition(reader, tagName, XMLStreamReader.END_ELEMENT);
+  }
+
+  private void readAndValidatePosition(XMLStreamReader reader, String tagName, int eventType) throws EntityConsumerException, XMLStreamException {
+    if(eventType != reader.next() || !reader.getLocalName().equals(tagName)) {
+      String msg = "Invalid position for expected name=" + tagName + " event='" + eventType +
+      		"'; found name='" + reader.getLocalName() + "' event='" + reader.getEventType() + "'.";
+      throw new EntityConsumerException(EntityConsumerException.INVALID_STATE.addContent(msg));
+    }
+  }
+  
+  private void validatePosition(XMLStreamReader reader, String tagName, int eventType) throws EntityConsumerException {
+    if(eventType != reader.getEventType() || !reader.getLocalName().equals(tagName)) {
+      String msg = "Invalid position for expected name=" + tagName + " event='" + eventType +
+          "'; found name='" + reader.getLocalName() + "' event='" + reader.getEventType() + "'.";
+      throw new EntityConsumerException(EntityConsumerException.INVALID_STATE.addContent(msg));
+    }
+  }
+
+  private Object readContent(XMLStreamReader reader, EdmEntitySet entitySet) throws Exception {
+    validateStartPosition(reader, "content");
+    
+    Map<String, String> attributes = readAttributes(reader);
+    
+    int nextEventType = reader.nextTag();
+    
+    if(XMLStreamReader.START_ELEMENT == nextEventType) {
+      Map<String, Object> result = new HashMap<String, Object>();
+      result.put("attributes", attributes);
+      result.put(reader.getLocalName(), readProperties(reader, entitySet));
+      return result;
+    } else if(XMLStreamReader.END_ELEMENT == nextEventType) {
+      validateEndPosition(reader, "content");
+      return attributes;
+    }
+    return null;
+  }
+
+  /**
+   * Read all attributes for current element to map (key=AttributeName; value=AttributeValue).
+   * 
+   * @param reader
+   * @return
+   */
+  private Map<String, String> readAttributes(XMLStreamReader reader) {
+    int attributesCount = reader.getAttributeCount();
+    
+    Map<String, String> attributes = new HashMap<String, String>();
+    for (int i = 0; i < attributesCount; i++) {
+      String name = reader.getAttributeName(i).getLocalPart();
+      String value = reader.getAttributeValue(i);
+      attributes.put(name, value);
+    }
+    return attributes;
+  }
+
+  private Object readCategory(XMLStreamReader reader) throws EntityConsumerException, XMLStreamException {
+    validateStartPosition(reader, "category");
+    Map<String, String> attributes = readAttributes(reader);
+    readAndValidateEndPosition(reader, "category");
+    
+    return attributes;
+  }
+
+  private Object readUpdated(XMLStreamReader reader) throws EntityConsumerException, XMLStreamException {
+    validateStartPosition(reader, "updated");
+    int eventType = reader.next();
+    Object value = null;
+    if(eventType == XMLStreamReader.CHARACTERS) {
+      value = reader.getText();
+    }
+    readAndValidateEndPosition(reader, "updated");
+    return value;
+  }
+
+  private Object readTitle(XMLStreamReader reader) throws EntityConsumerException, XMLStreamException {
+    validateStartPosition(reader, "title");
+    int eventType = reader.next();
+    Object value = null;
+    if(eventType == XMLStreamReader.CHARACTERS) {
+      value = reader.getText();
+    }
+    readAndValidateEndPosition(reader, "title");
+    return value;
+  }
+
+  private Object readId(XMLStreamReader reader) throws EntityConsumerException, XMLStreamException {
+    validateStartPosition(reader, "id");
+    int eventType = reader.next();
+    Object value = null;
+    if(eventType == XMLStreamReader.CHARACTERS) {
+      value = reader.getText();
+    }
+    readAndValidateEndPosition(reader, "id");
+    return value;
+  }
+
+  private Map<String, Object> readProperties(XMLStreamReader reader, EdmEntitySet entitySet) throws Exception {
     Map<String, Object> tagName2tagText = new HashMap<String, Object>();
     
     //
-    int nextTagEventType = readTillTag(reader, "properties");
-    //
+    int nextTagEventType = reader.next();
     XmlPropertyConsumer xpc = new XmlPropertyConsumer();
-    while((nextTagEventType = reader.next()) != XMLStreamReader.END_DOCUMENT) {
+    boolean run = true;
+    while(run) {
       if(nextTagEventType == XMLStreamReader.START_ELEMENT) {
         String name = reader.getLocalName();
         EdmProperty property = (EdmProperty) entitySet.getEntityType().getProperty(name);
         Object value = xpc.readStartedElement(reader, property);
         tagName2tagText.put(name, value);
+      } else if(nextTagEventType == XMLStreamReader.END_ELEMENT) {
+        String name = reader.getLocalName();
+        if("properties".equals(name)) {
+          run = false;
+        }
       }
+      nextTagEventType = reader.next();
     }
     
     return tagName2tagText;
-  }
-  
-
-  private int readTillTag(XMLStreamReader xmlStreamReader, String breakTagLocalName) throws XMLStreamException {
-    int nextTagEventType;
-    while((nextTagEventType = xmlStreamReader.next()) != XMLStreamReader.END_DOCUMENT) {
-      if(nextTagEventType == XMLStreamReader.START_ELEMENT) {
-        String localName = xmlStreamReader.getName().getLocalPart();
-        if(localName.equals(breakTagLocalName)) {
-          break;
-        }
-      }
-    }
-    return nextTagEventType;
   }
 }
