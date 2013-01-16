4a0bc68a143cce1c234b8d95f841e1fe77a96bfb
Stephan Klevenz <stephan.klevenz@sap.com>
2013-01-16 13:02:12 +0100

    deploy support
    Change-Id: I2c5bc681ceff403c44baa724c6d4b37844639d71

--- odata-web/deploy/nwcloud/deploy.ref.cmd
+++ odata-web/deploy/nwcloud/deploy.ref.cmd
@@ -1,7 +1,9 @@
 set HTTPS_PROXY_HOST=proxy
 set HTTPS_PROXY_PORT=8080
 
-call C:\.neosdk\neo-sdk-1.17.0\tools\neo.bat deploy -u p1160370 -password Test1234 odata-web\deploy\nwcloud\deploy.web.ref.odata.properties
-call C:\.neosdk\neo-sdk-1.17.0\tools\neo.bat stop -y -u p1160370 -password Test1234 odata-web\deploy\nwcloud\deploy.web.ref.odata.properties
-call C:\.neosdk\neo-sdk-1.17.0\tools\neo.bat start -y -u p1160370 -password Test1234 odata-web\deploy\nwcloud\deploy.web.ref.odata.properties
+rem NEO_SDK_HOME defined on Jenkins server!
+
+call %NEO_SDK_HOME%\tools\neo.bat deploy -u p1160370 -password Test1234 odata-web\deploy\nwcloud\deploy.web.ref.odata.properties
+call %NEO_SDK_HOME%\tools\neo.bat stop -y -u p1160370 -password Test1234 odata-web\deploy\nwcloud\deploy.web.ref.odata.properties
+call %NEO_SDK_HOME%\tools\neo.bat start -y -u p1160370 -password Test1234 odata-web\deploy\nwcloud\deploy.web.ref.odata.properties
                        
