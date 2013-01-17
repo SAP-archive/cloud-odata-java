5eeed1482fdaa44f6a3461bad2cae3617e033e8a
Michael Bolz <michael.bolz@sap.com>
2013-01-17 12:38:38 +0100

    Implemented XmlLinkConsumer
    Change-Id: I5409aed06288753e144806bed05ee5bb969e9aaf

--- odata-core/src/main/java/com/sap/core/odata/core/ep/consumer/XmlLinkConsumer.java
+++ odata-core/src/main/java/com/sap/core/odata/core/ep/consumer/XmlLinkConsumer.java
@@ -1,20 +1,125 @@
 package com.sap.core.odata.core.ep.consumer;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
 import com.sap.core.odata.api.edm.EdmEntitySet;
 import com.sap.core.odata.api.ep.EntityProviderException;
 
+/**
+ * 
+ */
 public class XmlLinkConsumer {
 
+  private static final String TAG_URI = "uri";
+  private static final String TAG_LINKS = "links";
+
+  /**
+   * Reads single link in format <code> <uri>http://somelink</uri> </code>
+   * 
+   * @param reader
+   * @param entitySet
+   * @return
+   * @throws EntityProviderException
+   */
   public String readLink(XMLStreamReader reader, EdmEntitySet entitySet) throws EntityProviderException {
-    return null;
+    try {
+      String link = readLink(reader);
+
+      return link;
+    } catch (Exception e) {
+      throw new EntityProviderException(EntityProviderException.COMMON, e);
+    }
   }
 
+  private String readLink(XMLStreamReader reader) throws EntityProviderException, XMLStreamException {
+    return readLink(reader, false);
+  }
+  
+  private String readLink(XMLStreamReader reader, boolean started) throws EntityProviderException, XMLStreamException {
+    //
+    int eventType = reader.getEventType();
+    if(!started) {
+      eventType = reader.next();
+    }
+    
+    if(eventType != XMLStreamReader.START_ELEMENT || !reader.getLocalName().equals(TAG_URI)) {
+      throw new EntityProviderException(EntityProviderException.INVALID_STATE
+          .addContent("Found no starting '" + TAG_URI + "' tag for link parsing."));
+    }
+    
+    //
+    String result = null;
+    eventType = reader.next();
+    if(XMLStreamReader.CHARACTERS == eventType) {
+      result = reader.getText();
+    } else {
+      throw new EntityProviderException(EntityProviderException.INVALID_STATE
+          .addContent("Found no text for '" + TAG_URI + "' tag for link parsing."));      
+    }
+    
+    //
+    eventType = reader.next();
+    if(eventType != XMLStreamReader.END_ELEMENT || !reader.getLocalName().equals(TAG_URI)) {
+      throw new EntityProviderException(EntityProviderException.INVALID_STATE
+          .addContent("Found no closing '" + TAG_URI + "' tag for link parsing."));
+    } 
+    return result;
+  }
+
+  /**
+   * Reads multiple links in format 
+   * <code>
+   * <pre>
+   * <links>
+   *  <uri>http://somelink</uri>
+   *  <uri>http://anotherLink</uri>
+   *  <uri>http://somelink/yetAnotherLink</uri>
+   * </links>
+   * </pre>
+   * </code>
+   * 
+   * @param reader
+   * @param entitySet
+   * @return
+   * @throws EntityProviderException
+   */
   public List<String> readLinks(XMLStreamReader reader, EdmEntitySet entitySet) throws EntityProviderException {
-    return Collections.emptyList();
+    try {
+      List<String > links = new ArrayList<String>();
+      
+      if(!startWithLinksTag(reader)) {
+        throw new EntityProviderException(EntityProviderException.INVALID_STATE
+            .addContent("Found no starting '" + TAG_LINKS + "' tag for link parsing."));
+      }
+      
+      while(linksEndNotReached(reader)) {
+        String link = readLink(reader, true);
+        links.add(link);
+      }
+
+      return links;
+    } catch (Exception e) {
+      throw new EntityProviderException(EntityProviderException.COMMON, e);
+    }
+  }
+
+  private boolean startWithLinksTag(XMLStreamReader reader) throws XMLStreamException {
+    int eventType = reader.next();
+    boolean isStartLinks = eventType == XMLStreamReader.START_ELEMENT && reader.getLocalName().equals(TAG_LINKS);
+    return isStartLinks;
+  }
+
+  private boolean isEndLinksTag(XMLStreamReader reader, int eventType) {
+    boolean isStartLinks = eventType != XMLStreamReader.END_ELEMENT || !reader.getLocalName().equals(TAG_LINKS);
+    return isStartLinks;
+  }
+
+  private boolean linksEndNotReached(XMLStreamReader reader) throws XMLStreamException {
+    int eventType = reader.next();
+    return isEndLinksTag(reader, eventType);
   }
 }
--- odata-core/src/test/java/com/sap/core/odata/core/ep/consumer/XmlLinkConsumerTest.java
+++ odata-core/src/test/java/com/sap/core/odata/core/ep/consumer/XmlLinkConsumerTest.java
@@ -10,37 +10,43 @@ import org.junit.Test;
 
 import com.sap.core.odata.api.edm.EdmEntitySet;
 
-public class XmlLinkConsumerTest {
+public class XmlLinkConsumerTest extends AbstractConsumerTest {
 
   // -> http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Managers('1')/$links/nm_Employees
   
-  String MANAGER_1_EMPLOYEES = 
+  private static final String MANAGER_1_EMPLOYEES = 
       "<links xmlns=\"http://schemas.microsoft.com/ado/2007/08/dataservices\">" +
         "<uri>http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('1')</uri>" +
         "<uri>http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('2')</uri>" +
         "<uri>http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('3')</uri>" +
         "<uri>http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('6')</uri>" +
       "</links>";
+
+  private static final String SINGLE_LINK = "<uri>http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('6')</uri>";
   
   @Test
   public void testReadLink() throws Exception {
     XmlLinkConsumer xlc = new XmlLinkConsumer();
     
-    XMLStreamReader reader = null;
+    XMLStreamReader reader = createReaderForTest(SINGLE_LINK);
     EdmEntitySet entitySet = null;
     String link = xlc.readLink(reader, entitySet);
 
-    assertEquals(null, link);
+    assertEquals("http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('6')", link);
   }
 
   @Test
   public void testReadLinks() throws Exception {
     XmlLinkConsumer xlc = new XmlLinkConsumer();
     
-    XMLStreamReader reader = null;
+    XMLStreamReader reader = createReaderForTest(MANAGER_1_EMPLOYEES);
     EdmEntitySet entitySet = null;
     List<String> links = xlc.readLinks(reader, entitySet);
 
-    assertEquals(0, links.size());
+    assertEquals(4, links.size());
+    assertEquals("http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('1')", links.get(0));
+    assertEquals("http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('2')", links.get(1));
+    assertEquals("http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('3')", links.get(2));
+    assertEquals("http://ldcigmd.wdf.sap.corp:50055/sap/bc/odata/Employees('6')", links.get(3));
   }
 }
