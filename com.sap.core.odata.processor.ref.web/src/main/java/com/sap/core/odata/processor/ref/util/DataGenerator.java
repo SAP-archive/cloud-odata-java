a05a84d52059b697bab414b328253ec20a868100
Santosh Kumar <santosh.kumar04@sap.com>
2013-01-18 14:05:45 +0530

    Updated Data generator
    Change-Id: I043cd915cf3d759175231e7625f72e9b0010e0b0
    Signed-off-by: Santosh Kumar <santosh.kumar04@sap.com>

--- com.sap.core.odata.processor.ref.web/src/main/java/com/sap/core/odata/processor/ref/util/DataGenerator.java
+++ com.sap.core.odata.processor.ref.web/src/main/java/com/sap/core/odata/processor/ref/util/DataGenerator.java
@@ -6,16 +6,18 @@ import java.util.ResourceBundle;
 import java.util.Set;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 
 import org.eclipse.persistence.internal.jpa.EntityManagerImpl;
 import org.eclipse.persistence.queries.DataModifyQuery;
 import org.eclipse.persistence.queries.SQLCall;
 import org.eclipse.persistence.sessions.Session;
 
-import com.sap.core.odata.processor.ref.jpa.SalesOrderHeader;
-import com.sap.core.odata.processor.ref.jpa.SalesOrderItem;
-
+/**
+ * This is a utility class for generating and cleaning data. The generated data would be used by the application.
+ * 
+ * @author SAP AG
+ *
+ */
 public class DataGenerator {
 
 	private EntityManager entityManager;
@@ -37,16 +39,14 @@ public class DataGenerator {
 				Set<String> keySet = resourceBundleArr[i].keySet();
 				
 				for (Iterator<String> iterator = keySet.iterator(); iterator.hasNext();) {
-					//this.entityManager.getTransaction().begin();
 					String currentSQL = (String) iterator.next();
 					String sqlQuery = resourceBundleArr[i].getString(currentSQL);
-					System.out.println("Currently executing Query - "+sqlQuery);
+					System.out.println("Executing Query - "+sqlQuery);
 					SQLCall sqlCall = new SQLCall(sqlQuery);
 					
 					DataModifyQuery query = new DataModifyQuery();
 					query.setCall(sqlCall);
 					session.executeQuery(query);
-					//this.entityManager.getTransaction().commit();
 				}
 			}
 			this.entityManager.getTransaction().commit();
@@ -58,43 +58,28 @@ public class DataGenerator {
 	}
 	
 
+	/**
+	 * This method deletes data from JPA tables created.
+	 */
 	public void clean() {
 		this.entityManager.getTransaction().begin();
-
-		/*TypedQuery<SalesOrderItem> querySalesOrderItem = this.entityManager.createQuery(
-				"SELECT m FROM SalesOrderItem m", SalesOrderItem.class);
-		for (SalesOrderItem part : querySalesOrderItem.getResultList()) {
-			this.entityManager.remove(part);
-		}
-		TypedQuery<SalesOrderHeader> querySalesOrder = this.entityManager
-				.createQuery("SELECT m FROM SalesOrderHeader m",
-						SalesOrderHeader.class);
-		for (SalesOrderHeader whole : querySalesOrder.getResultList()) {
-			this.entityManager.remove(whole);
-		}*/
-		
-		
 		
 		// Delete using SQLs
 		Session session = ((EntityManagerImpl) entityManager).getActiveSession();
 		ResourceBundle resourceBundle = ResourceBundle.getBundle("DataDeleteSQLs");//Get SQL statements as properties
 		
 		Set<String> keySet = resourceBundle.keySet();
-		//session.
 		
 		for (Iterator<String> iterator = keySet.iterator(); iterator.hasNext();) {
 			String currentSQL = (String) iterator.next();
 			String sqlQuery = resourceBundle.getString(currentSQL);
-			System.out.println("In Clean(), executing Query - "+sqlQuery);
+			System.out.println("Cleaning - "+sqlQuery);
 			SQLCall sqlCall = new SQLCall(sqlQuery);
 			
 			DataModifyQuery query = new DataModifyQuery();
 			query.setCall(sqlCall);
 			session.executeQuery(query);
 		}
-		
-		
-		
 		this.entityManager.getTransaction().commit();
 	}
 
