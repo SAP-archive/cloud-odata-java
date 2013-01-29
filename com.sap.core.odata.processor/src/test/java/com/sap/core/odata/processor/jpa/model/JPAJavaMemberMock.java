ddf7a3725a4a73b0f55924954a1e7fa36854a8a5
Santosh Kumar <santosh.kumar04@sap.com>
2013-01-29 15:30:18 +0530

    Added JUnits for JPAEdmReferentialConstraintRole. Change-Id: I1ac35c87ee8e7ab3fe90f79b3040e595ef05aeda Signed-off-by: Santosh Kumar <santosh.kumar04@sap.com>

---
+++ com.sap.core.odata.processor/src/test/java/com/sap/core/odata/processor/jpa/model/JPAJavaMemberMock.java
@@ -0,0 +1,65 @@
+package com.sap.core.odata.processor.jpa.model;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Member;
+
+import javax.persistence.JoinColumns;
+
+public class JPAJavaMemberMock implements Member, AnnotatedElement, Annotation {
+
+	@Override
+	public Class<?> getDeclaringClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getModifiers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isSynthetic() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAnnotationPresent(
+			Class<? extends Annotation> annotationClass) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+		@Override
+	public Annotation[] getAnnotations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Annotation[] getDeclaredAnnotations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends Annotation> annotationType() {
+		return JoinColumns.class;
+	}
+
+	@Override
+	public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ com.sap.core.odata.processor/src/test/java/com/sap/core/odata/processor/jpa/model/JPAManagedTypeMock.java
@@ -0,0 +1,202 @@
+package com.sap.core.odata.processor.jpa.model;
+
+import java.util.Set;
+
+import javax.persistence.metamodel.Attribute;
+import javax.persistence.metamodel.CollectionAttribute;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.ManagedType;
+import javax.persistence.metamodel.MapAttribute;
+import javax.persistence.metamodel.PluralAttribute;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+
+public class JPAManagedTypeMock<X> implements ManagedType<X> {
+
+	@Override
+	public Class<X> getJavaType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public javax.persistence.metamodel.Type.PersistenceType getPersistenceType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Attribute<? super X, ?> getAttribute(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Attribute<? super X, ?>> getAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CollectionAttribute<? super X, ?> getCollection(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E> CollectionAttribute<? super X, E> getCollection(String arg0,
+			Class<E> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Attribute<X, ?> getDeclaredAttribute(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Attribute<X, ?>> getDeclaredAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CollectionAttribute<X, ?> getDeclaredCollection(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E> CollectionAttribute<X, E> getDeclaredCollection(String arg0,
+			Class<E> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListAttribute<X, ?> getDeclaredList(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E> ListAttribute<X, E> getDeclaredList(String arg0, Class<E> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MapAttribute<X, ?, ?> getDeclaredMap(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <K, V> MapAttribute<X, K, V> getDeclaredMap(String arg0,
+			Class<K> arg1, Class<V> arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<PluralAttribute<X, ?, ?>> getDeclaredPluralAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SetAttribute<X, ?> getDeclaredSet(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E> SetAttribute<X, E> getDeclaredSet(String arg0, Class<E> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SingularAttribute<X, ?> getDeclaredSingularAttribute(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <Y> SingularAttribute<X, Y> getDeclaredSingularAttribute(
+			String arg0, Class<Y> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<SingularAttribute<X, ?>> getDeclaredSingularAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListAttribute<? super X, ?> getList(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E> ListAttribute<? super X, E> getList(String arg0, Class<E> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MapAttribute<? super X, ?, ?> getMap(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <K, V> MapAttribute<? super X, K, V> getMap(String arg0,
+			Class<K> arg1, Class<V> arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<PluralAttribute<? super X, ?, ?>> getPluralAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SetAttribute<? super X, ?> getSet(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E> SetAttribute<? super X, E> getSet(String arg0, Class<E> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SingularAttribute<? super X, ?> getSingularAttribute(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <Y> SingularAttribute<? super X, Y> getSingularAttribute(
+			String arg0, Class<Y> arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<SingularAttribute<? super X, ?>> getSingularAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
