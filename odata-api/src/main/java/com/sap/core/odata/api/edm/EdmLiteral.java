7d9170975c23d9a6045974c85c22e6b9cb9d6748
Klaus Straubinger <klaus.straubinger@sap.com>
2013-02-01 09:25:42 +0100

    ODATAFORSAP-14
    Change-Id: Ice3b3f5514a98f2335d043b77616920c6b3bfe7e

--- odata-api/src/main/java/com/sap/core/odata/api/edm/EdmLiteral.java
+++ odata-api/src/main/java/com/sap/core/odata/api/edm/EdmLiteral.java
@@ -1,17 +1,24 @@
 package com.sap.core.odata.api.edm;
 
-/** 
- * UriLiteral representation
+/**
+ * <p>Representation of a simple-typed literal</p>
+ * <p>The literal is in default representation. The URI representation differs
+ * from the default representation mainly in the additional presence of type
+ * indicators (prefixes or suffixes, respectively); since the type information
+ * is stored here separately, the default representation is more appropriate.
+ * Should the URI representation be needed, it can be re-created by calling
+ * {@link EdmSimpleType#toUriLiteral}.</p>
  * @author SAP AG
+ * @see EdmLiteralKind
  */
 public final class EdmLiteral {
   private final EdmSimpleType type;
   private final String literal;
 
   /**
+   * Creates an {@link EdmLiteral} object out of the simple type and the literal string.
    * @param type {@link EdmSimpleType} simple type
-   * @param literal {@link String} literal in default (<em>not</em> URI) representation
-   * @see EdmLiteralKind
+   * @param literal String literal in default (<em>not</em> URI) representation
    */
   public EdmLiteral(final EdmSimpleType type, final String literal) {
     this.type = type;
@@ -19,6 +26,7 @@ public final class EdmLiteral {
   }
 
   /**
+   * Gets the simple type of the literal.
    * @return {@link EdmSimpleType} object
    */
   public EdmSimpleType getType() {
@@ -26,6 +34,7 @@ public final class EdmLiteral {
   }
 
   /**
+   * Gets the literal String.
    * @return {@link String} literal in default (<em>not</em> URI) representation
    */
   public String getLiteral() {
@@ -34,6 +43,6 @@ public final class EdmLiteral {
 
   @Override
   public String toString() {
-    return "UriLiteral: type=" + type + ", literal=" + literal;
+    return "type=" + type + ", literal=" + literal;
   }
 }
