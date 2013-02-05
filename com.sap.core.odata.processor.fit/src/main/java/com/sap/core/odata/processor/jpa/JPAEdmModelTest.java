4e82b96c0d2a60b99e86f8a0aa81f5bbb87dd102
Anirban Roy <anirban.roy@sap.com>
2013-02-05 16:32:00 +0530

    Content added to FIT Change-Id: I77c8509fd39bb8c5e84ed5a92553f56df572339c Signed-off-by: Anirban Roy <anirban.roy@sap.com>

---
+++ com.sap.core.odata.processor.fit/src/main/java/com/sap/core/odata/processor/jpa/JPAEdmModelTest.java
@@ -0,0 +1,37 @@
+package com.sap.core.odata.processor.jpa;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.sap.core.odata.api.edm.provider.Schema;
+import com.sap.core.odata.processor.jpa.api.exception.ODataJPAModelException;
+import com.sap.core.odata.processor.jpa.api.model.JPAEdmSchemaView;
+import com.sap.core.odata.processor.jpa.model.JPAEdmModel;
+
+public class JPAEdmModelTest {
+	
+	private static JPAEdmModel jpaEdmModel;
+	private static MockModel model;
+	
+	@BeforeClass
+	  public static void setup() throws Exception {
+		 
+		model = new MockModel();
+		jpaEdmModel = new JPAEdmModel(model.getMetaModel(), model.getPersistentUnitName());
+		
+	}
+
+	@Test
+	public void testGetSchemaView() throws ODataJPAModelException {
+		jpaEdmModel.getBuilder().build();
+		JPAEdmSchemaView schemaView = jpaEdmModel.getEdmSchemaView();
+		Schema schema = schemaView.getEdmSchema();
+		assertEquals(schema.getNamespace(), model.getPersistentUnitName());
+		
+	}
+
+	
+
+}
