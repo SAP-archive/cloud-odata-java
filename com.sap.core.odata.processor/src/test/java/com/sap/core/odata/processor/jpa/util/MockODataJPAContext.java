e6e089421ae2f92caa8a6a6c2a07265561ac259c
Chitresh Chauhan <chitresh.chauhan@sap.com>
2013-02-05 16:08:39 +0530

    model testcases Change-Id: I84f71ca73331214cae0050aef168e0751bed9427 Signed-off-by: Chitresh Chauhan <chitresh.chauhan@sap.com>

---
+++ com.sap.core.odata.processor/src/test/java/com/sap/core/odata/processor/jpa/util/MockODataJPAContext.java
@@ -0,0 +1,34 @@
+package com.sap.core.odata.processor.jpa.util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.metamodel.Metamodel;
+
+import org.easymock.EasyMock;
+
+import com.sap.core.odata.processor.jpa.api.ODataJPAContext;
+
+public class MockODataJPAContext {
+	
+	private static final String NAMESPACE = "salesorderprocessing";  
+	public static ODataJPAContext mockODataJPAContext()
+	{
+		ODataJPAContext odataJPAContext = EasyMock.createMock(ODataJPAContext.class);
+		EasyMock.expect(odataJPAContext.getPersistenceUnitName()).andReturn(NAMESPACE).times(2);
+		EasyMock.expect(odataJPAContext.getEntityManagerFactory()).andReturn(mockEntityManagerFactory());
+		EasyMock.replay(odataJPAContext);
+		return odataJPAContext;
+	}
+	private static EntityManagerFactory mockEntityManagerFactory() {
+		EntityManagerFactory emf = EasyMock.createMock(EntityManagerFactory.class);
+		EasyMock.expect(emf.getMetamodel()).andReturn(mockMetaModel());
+		EasyMock.replay(emf);
+		return emf;
+	}
+	private static Metamodel mockMetaModel() {
+		Metamodel metaModel = EasyMock.createMock(Metamodel.class);
+		EasyMock.replay(metaModel);
+		return metaModel;
+	}
+	
+
+}
