1d05b43883510365ceda779641440c19c89fbf16
Christian Amend <christian.amend@sap.com>
2013-02-20 09:17:23 +0100

    Javadoc uri package minor adjustments
    Change-Id: Ibe60697090715a39d866e2409b59150518f4a283

--- odata-api/src/main/java/com/sap/core/odata/api/uri/info/package-info.java
+++ odata-api/src/main/java/com/sap/core/odata/api/uri/info/package-info.java
@@ -1,6 +1,6 @@
 /**
  * <p>URI Parser Result Views</p>
- * <p>The interfaces of this package provide access to the relevant parts of the
- * request URI for the methods that process the requests.</p>
+ * <p>The interfaces of this package provide access to the relevant parts of the parsed
+ * request URI for the methods that process a request.</p>
  */
 package com.sap.core.odata.api.uri.info;
--- odata-api/src/main/java/com/sap/core/odata/api/uri/package-info.java
+++ odata-api/src/main/java/com/sap/core/odata/api/uri/package-info.java
@@ -1,6 +1,8 @@
 /**
  * <p>URI Parser Facade</p>
- * <p>The URI package has one central class to parse a request URI
- * as well as several interfaces that provide access to different parts of the URI.</p>
+ * <p>The URI package has one central class {@link com.sap.core.odata.api.uri.UriParser}  to parse a request URI
+ * as well as several interfaces that provide access to parsed parts of the URI.
+ * <br>The {@link com.sap.core.odata.api.uri.UriParser} class also provides the possibility to parse a filter or an orderBy Statement. Both are specified in the OData Protocol Specification. 
+ * <br>the URI syntax is specified in the OData Protocol Specification in the form of an ABNF. </p>
  */
 package com.sap.core.odata.api.uri;
