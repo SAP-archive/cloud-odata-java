981f133f55d1a02f044fe2c45596262727fc3eec
Chandan V A <chandan.v.a@sap.com>
2013-03-05 17:52:19 +0530

    Package Info for Mapping Model
    Change-Id: Iff30ddccf0922da06178016e5a8ba842b6a02574
    Signed-off-by: Chandan V A <chandan.v.a@sap.com>

--- com.sap.core.odata.processor.api/src/main/java/com/sap/core/odata/processor/api/jpa/model/mapping/package-info.java
+++ com.sap.core.odata.processor.api/src/main/java/com/sap/core/odata/processor/api/jpa/model/mapping/package-info.java
@@ -1,3 +1,9 @@
+/**
+ * <h3>OData JPA Processor API Library - Mapping Model</h3>
+ * The JPA EDM Mapping model (XML document) is represented as JAXB annotated Java Classes.
+ * 
+ * @author SAP AG
+ */
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.sap.com/core/odata/processor/api/jpa/model/mapping", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.sap.core.odata.processor.api.jpa.model.mapping;
 
