6f668fe4c22683e524dfbbd7faf94cf0b66db71b
Christian Amend <christian.amend@sap.com>
2013-03-07 18:03:37 +0100

    Further implementation of expand select tree
    Functionallity there. Some tests still missing. Also still needs deletion
    of unused comments.
    
    Change-Id: I3a6332286dcebe93a288531d2aa248e8bc95f331

--- odata-core/src/main/java/com/sap/core/odata/core/uri/ExpandSelectTreeCreatorImpl.java
+++ odata-core/src/main/java/com/sap/core/odata/core/uri/ExpandSelectTreeCreatorImpl.java
@@ -1,14 +1,14 @@
 package com.sap.core.odata.core.uri;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import com.sap.core.odata.api.edm.EdmNavigationProperty;
+import com.sap.core.odata.api.edm.EdmException;
 import com.sap.core.odata.api.uri.ExpandSelectTreeCreator;
 import com.sap.core.odata.api.uri.ExpandSelectTreeNode;
 import com.sap.core.odata.api.uri.NavigationPropertySegment;
 import com.sap.core.odata.api.uri.SelectItem;
-import com.sap.core.odata.core.exception.ODataRuntimeException;
 
 public class ExpandSelectTreeCreatorImpl implements ExpandSelectTreeCreator {
 
@@ -19,13 +19,13 @@ public class ExpandSelectTreeCreatorImpl implements ExpandSelectTreeCreator {
     if (select != null && select.isEmpty() == false) {
       initialSelect = select;
     } else {
-      initialSelect = null;
+      initialSelect = Collections.emptyList();
     }
 
     if (expand != null && expand.isEmpty() == false) {
       initialExpand = expand;
     } else {
-      initialExpand = null;
+      initialExpand = Collections.emptyList();
     }
   }
 
@@ -34,13 +34,36 @@ public class ExpandSelectTreeCreatorImpl implements ExpandSelectTreeCreator {
 
     //Initial node
     ExpandSelectTreeNodeImpl root = new ExpandSelectTreeNodeImpl();
-    if (initialSelect != null && initialExpand != null) {
-      buildCombinedTree(root);
-    } else if (initialSelect != null && initialExpand == null) {
-      buildPureSelectTree(root);
-    } else if (initialSelect == null && initialExpand != null) {
+    if (initialSelect.isEmpty() && !initialExpand.isEmpty()) {
       buildPureExpandTree(root);
+    } else {
+      try {
+        buildCombinedTree(root);
+      } catch (EdmException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
     }
+
+    //    if (!initialSelect.isEmpty() && !initialExpand.isEmpty()) {
+    //      try {
+    //        buildCombinedTree(root);
+    //      } catch (EdmException e) {
+    //        // TODO Auto-generated catch block
+    //        e.printStackTrace();
+    //      }
+    //    } else if (!initialSelect.isEmpty() && initialExpand.isEmpty()) {
+    //      try {
+    //        buildCombinedTree(root);
+    //      } catch (EdmException e) {
+    //        // TODO Auto-generated catch block
+    //        e.printStackTrace();
+    //      }
+    //      
+    //      //buildPureSelectTree(root);
+    //    } else if (initialSelect.isEmpty() && !initialExpand.isEmpty()) {
+    //      buildPureExpandTree(root);
+    //    }
 
     return root;
   }
@@ -59,26 +82,119 @@ public class ExpandSelectTreeCreatorImpl implements ExpandSelectTreeCreator {
 
   }
 
-  private void buildPureSelectTree(final ExpandSelectTreeNodeImpl root) {
+  //  private void buildPureSelectTree(final ExpandSelectTreeNodeImpl root) {
+  //    for (SelectItem item : initialSelect) {
+  //      if (item.getNavigationPropertySegments().isEmpty()) {
+  //        if (item.getProperty() != null) {
+  //          root.addProperty(item.getProperty());
+  //        } else if (item.isStar()) {
+  //          root.setAllExplicitly();
+  //        } else {
+  //          throw new ODataRuntimeException("Empty select item are not allowed!");
+  //        }
+  //      } else {
+  //        EdmNavigationProperty navProperty = item.getNavigationPropertySegments().get(0).getNavigationProperty();
+  //        root.addChild(navProperty, null);
+  //      }
+  //    }
+  //  }
+
+  private void buildCombinedTree(final ExpandSelectTreeNodeImpl root) throws EdmException {
     for (SelectItem item : initialSelect) {
-      if (item.getNavigationPropertySegments().isEmpty()) {
-        if (item.getProperty() != null) {
-          root.addProperty(item.getProperty());
-        } else if (item.isStar()) {
-          root.setAllExplicitly();
-        } else {
-          throw new ODataRuntimeException("Empty select item are not allowed!");
+      List<NavigationPropertySegment> segmentsList = item.getNavigationPropertySegments();
+      ExpandSelectTreeNodeImpl actualNode = root;
+      for (int segmentListIndex = 0; segmentListIndex < segmentsList.size(); segmentListIndex++) {
+        ExpandSelectTreeNodeImpl childNode = null;
+        for (ArrayList<NavigationPropertySegment> singleExpandList : initialExpand) {
+          if (singleExpandList.size() > segmentListIndex) {
+            boolean ok = true;
+            for (int expandListIndex = 0; expandListIndex <= segmentListIndex; expandListIndex++) {
+              if (!segmentsList.get(expandListIndex).getNavigationProperty().getName().equals(singleExpandList.get(expandListIndex).getNavigationProperty().getName())) {
+                ok = false;
+                break;
+              }
+            }
+
+            if (ok) {
+              childNode = new ExpandSelectTreeNodeImpl();
+              if ((item.getProperty() == null && !item.isStar()) && segmentListIndex == segmentsList.size() - 1) {
+                ExpandSelectTreeNodeImpl expandNodes = childNode;
+                for (int expandListIndex = segmentListIndex + 1; expandListIndex < singleExpandList.size(); expandListIndex++) {
+                  ExpandSelectTreeNode newNode = new ExpandSelectTreeNodeImpl();
+                  expandNodes.setAllExplicitly();
+                  expandNodes = (ExpandSelectTreeNodeImpl) expandNodes.addChild(singleExpandList.get(expandListIndex).getNavigationProperty(), newNode);
+                }
+              }
+              break;
+            }
+          }
         }
-      } else {
-        EdmNavigationProperty navProperty = item.getNavigationPropertySegments().get(0).getNavigationProperty();
-        root.addChild(navProperty, null);
+        actualNode = (ExpandSelectTreeNodeImpl) actualNode.addChild(segmentsList.get(segmentListIndex).getNavigationProperty(), childNode);
+        if (actualNode == null) {
+          break;
+        }
       }
+
+      if (actualNode != null) {
+        if (item.getProperty() != null) {
+          actualNode.addProperty(item.getProperty());
+        } else if (item.isStar()) {
+          actualNode.setAllExplicitly();
+        }
+      }
+
     }
   }
 
-  private void buildCombinedTree(final ExpandSelectTreeNodeImpl root) {
-    // TODO Auto-generated method stub
-
-  }
-
 }
+
+//  private void buildCombinedTree(final ExpandSelectTreeNodeImpl root) throws EdmException {
+//    for (SelectItem item : initialSelect) {
+//      if (item.getNavigationPropertySegments().isEmpty()) {
+//        if (item.getProperty() != null) {
+//          root.addProperty(item.getProperty());
+//        } else if (item.isStar()) {
+//          root.setAllExplicitly();
+//        } else {
+//          throw new ODataRuntimeException("Empty select item are not allowed!");
+//        }
+//      } else {
+//        List<NavigationPropertySegment> segmentsList = item.getNavigationPropertySegments();
+//        ExpandSelectTreeNodeImpl actualNode = root;
+//        for (int segmentListIndex = 0; segmentListIndex < segmentsList.size(); segmentListIndex++) {
+//          ExpandSelectTreeNodeImpl childNode = null;
+//          for (ArrayList<NavigationPropertySegment> singleExpandList : initialExpand) {
+//            if (singleExpandList.size() > segmentListIndex) {
+//              boolean ok = true;
+//              for (int expandListIndex = 0; expandListIndex <= segmentListIndex; expandListIndex++) {
+//                if (!segmentsList.get(expandListIndex).getNavigationProperty().getName().equals(singleExpandList.get(expandListIndex).getNavigationProperty().getName())) {
+//                  ok = false;
+//                  break;
+//                }
+//              }
+//
+//              if (ok) {
+//                childNode = new ExpandSelectTreeNodeImpl();
+//                break;
+//              }
+//            }
+//          }
+//          actualNode = (ExpandSelectTreeNodeImpl) actualNode.addChild(segmentsList.get(segmentListIndex).getNavigationProperty(), childNode);
+//          if (actualNode == null) {
+//            break;
+//          }
+//
+//          if (segmentListIndex == segmentsList.size()) {
+//            if (item.getProperty() != null) {
+//              actualNode.addProperty(item.getProperty());
+//            } else if (item.isStar()) {
+//              actualNode.setAllExplicitly();
+//            }
+//          }
+//        }
+//
+//      }
+//    }
+//
+//  }
+
