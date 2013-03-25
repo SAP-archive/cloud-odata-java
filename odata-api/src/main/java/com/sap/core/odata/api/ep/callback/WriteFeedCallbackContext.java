9ed52c74b6314935b83a6296dacc55bbb0aabf90
Christian Amend <christian.amend@sap.com>
2013-03-25 10:29:30 +0100

    Add entry data to context
    Entry data was added to context and a util method to extract the key data
    was introduced
    
    Change-Id: Ie539ed401fd2cb0bd0910c4eeecb13ac38dddb00

--- odata-api/src/main/java/com/sap/core/odata/api/ep/callback/WriteFeedCallbackContext.java
+++ odata-api/src/main/java/com/sap/core/odata/api/ep/callback/WriteFeedCallbackContext.java
@@ -1,38 +1,6 @@
 package com.sap.core.odata.api.ep.callback;
 
-import java.util.Map;
 
-import com.sap.core.odata.api.edm.EdmEntitySet;
-import com.sap.core.odata.api.edm.EdmNavigationProperty;
+public class WriteFeedCallbackContext extends WrtiteCallbackContext {
 
-public class WriteFeedCallbackContext {
- private EdmEntitySet sourceEntitySet;
-  
-  private EdmNavigationProperty navigationProperty;
-  
-  private Map<String, Object> key;
-
-  public EdmEntitySet getSourceEntitySet() {
-    return sourceEntitySet;
-  }
-
-  public void setSourceEntitySet(EdmEntitySet entitySet) {
-    this.sourceEntitySet = entitySet;
-  }
-
-  public EdmNavigationProperty getNavigationProperty() {
-    return navigationProperty;
-  }
-
-  public void setNavigationProperty(EdmNavigationProperty navigationProperty) {
-    this.navigationProperty = navigationProperty;
-  }
-
-  public Map<String, Object> getKey() {
-    return key;
-  }
-
-  public void setKey(Map<String, Object> key) {
-    this.key = key;
-  }
 }
