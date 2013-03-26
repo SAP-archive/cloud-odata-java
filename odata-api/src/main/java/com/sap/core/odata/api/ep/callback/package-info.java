fb810be235a9ecd8a35fdab3d4c6276559ba5af5
Christian Amend <christian.amend@sap.com>
2013-03-26 09:53:00 +0100

    Javadoc callbacks final
    Change-Id: Id4a37865ef508bc31fc03d79fe5f4c23d6c93352

--- odata-api/src/main/java/com/sap/core/odata/api/ep/callback/package-info.java
+++ odata-api/src/main/java/com/sap/core/odata/api/ep/callback/package-info.java
@@ -1,6 +1,9 @@
 /**
  * Entity Provider Callbacks<p>
+ * These callbacks will be used to support the $expand query option. Callbacks have to implement the {@link com.sap.core.odata.api.ODataCallback} as a marker. 
+ * <br>To support an expanded entry the {@link com.sap.core.odata.api.ep.callback.OnWriteEntryContent} interface has to be implemented.
+ * <br>To support an expanded feed the {@link com.sap.core.odata.api.ep.callback.OnWriteFeedContent} interface has to be implemented.
  * 
-
+ * <p>All callbacks are registered for a navigation property in a HashMap<String as navigation property name, callback for this navigation property> and will only be called if a matching $expand clause is found.
  */
 package com.sap.core.odata.api.ep.callback;
