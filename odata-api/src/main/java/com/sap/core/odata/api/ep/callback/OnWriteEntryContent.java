8fd005904ba0790b98bcdd3560811fd5d5709b29
Christian Amend <christian.amend@sap.com>
2013-03-26 08:18:57 +0100

    Javadoc callbacks first draft
    Change-Id: I032648616c8d2f7e4f8153f8c3a0d425e56cbc3f

--- odata-api/src/main/java/com/sap/core/odata/api/ep/callback/OnWriteEntryContent.java
+++ odata-api/src/main/java/com/sap/core/odata/api/ep/callback/OnWriteEntryContent.java
@@ -1,7 +1,20 @@
 package com.sap.core.odata.api.ep.callback;
 
+/**
+ * Callback interface for the $expand query option. 
+ * <p>If an expand clause for a navigation property which points to an entry is found this callback will be called.
+ * <br>Pointing to an entry means the navigation property has a multiplicity of 0..1 or 1..1.
+ * 
+ * @author SAP AG
+ *
+ */
 public interface OnWriteEntryContent {
   
+  /**
+   * Retrieves the data for an entry. See {@link WriteEntryCallbackContext} for details on the context and {@link WriteEntryCallbackResult} for details on the result of this method.
+   * @param context of this entry
+   * @return result - must not be null.
+   */
   WriteEntryCallbackResult retrieveEntryResult(WriteEntryCallbackContext context);
 
 }
--- odata-api/src/main/java/com/sap/core/odata/api/ep/callback/OnWriteFeedContent.java
+++ odata-api/src/main/java/com/sap/core/odata/api/ep/callback/OnWriteFeedContent.java
@@ -1,7 +1,20 @@
 package com.sap.core.odata.api.ep.callback;
 
+/**
+ * Callback interface for the $expand query option. 
+ * <p>If an expand clause for a navigation property which points to a feed is found this callback will be called.
+ * <br>Pointing to an feed means the navigation property has a multiplicity of 0..* or 1..*.
+ * 
+ * @author SAP AG
+ *
+ */
 public interface OnWriteFeedContent {
 
+  /**
+   * Retrieves the data for a feed. See {@link WriteFeedCallbackContext} for details on the context and {@link WriteFeedCallbackResult} for details on the result of this method.
+   * @param context of this entry
+   * @return result - must not be null.
+   */
   WriteFeedCallbackResult retrieveFeedResult(WriteFeedCallbackContext context);
   
 }
