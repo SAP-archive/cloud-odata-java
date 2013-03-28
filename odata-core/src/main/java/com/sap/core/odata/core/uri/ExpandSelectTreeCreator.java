4e54d454805b77c6fb854cffcc2afbf7a8735738
Christian Amend <christian.amend@sap.com>
2013-03-28 12:44:01 +0100

    Improved ExpandSelectTreecreator
    Change-Id: I1dd83f2da3a3d1e8e564caaeeb2bb457bae4f199

--- odata-core/src/main/java/com/sap/core/odata/core/uri/ExpandSelectTreeCreator.java
+++ odata-core/src/main/java/com/sap/core/odata/core/uri/ExpandSelectTreeCreator.java
@@ -2,21 +2,20 @@ package com.sap.core.odata.core.uri;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import com.sap.core.odata.api.edm.EdmException;
 import com.sap.core.odata.api.uri.ExpandSelectTreeNode;
 import com.sap.core.odata.api.uri.NavigationPropertySegment;
 import com.sap.core.odata.api.uri.SelectItem;
+import com.sap.core.odata.core.uri.ExpandSelectTreeNodeImpl.AllKinds;
 
-/**
- * Creates an optimized combined expression tree out of the expand and select options.
- * @author SAP AG
- */
 public class ExpandSelectTreeCreator {
 
-  private final List<SelectItem> initialSelect;
-  private final List<ArrayList<NavigationPropertySegment>> initialExpand;
+  private List<SelectItem> initialSelect;
+  private List<ArrayList<NavigationPropertySegment>> initialExpand;
 
   public ExpandSelectTreeCreator(final List<SelectItem> select, final List<ArrayList<NavigationPropertySegment>> expand) {
     if (select != null) {
@@ -36,75 +35,130 @@ public class ExpandSelectTreeCreator {
 
     //Initial node
     ExpandSelectTreeNodeImpl root = new ExpandSelectTreeNodeImpl();
-    if (initialSelect.isEmpty() && !initialExpand.isEmpty()) {
-      buildPureExpandTree(root);
+    if (!initialSelect.isEmpty()) {
+      //Create a full expand tree
+      createSelectTree(root);
     } else {
-      buildCombinedTree(root);
+      //If no select is given the root node is explicitly selected for all expand clauses
+      root.setExplicitlySeleced();
     }
+    //Merge in the expand tree
+    mergeExpandTree(root);
+
+    //consolidate the tree
+    consolidate(root);
     return root;
   }
 
-  private void buildPureExpandTree(final ExpandSelectTreeNodeImpl root) throws EdmException {
-    root.setAllExplicitly();
-    ExpandSelectTreeNodeImpl actualNode;
-    for (ArrayList<NavigationPropertySegment> navigationPropertySegmentList : initialExpand) {
-      actualNode = root;
-      for (NavigationPropertySegment segment : navigationPropertySegmentList) {
-        ExpandSelectTreeNodeImpl childNode = new ExpandSelectTreeNodeImpl();
-        childNode.setAllExplicitly();
-        actualNode = (ExpandSelectTreeNodeImpl) actualNode.addChild(segment.getNavigationProperty().getName(), childNode);
-      }
+  private void consolidate(final ExpandSelectTreeNodeImpl node) {
+    switch (node.getAllKind()) {
+    case EXPLICITLYTRUE:
+    case IMPLICITLYTRUE:
+      consolidateTrueNode(node);
+      break;
+    case FALSE:
+      consolidateFalseNode(node);
     }
 
   }
 
-  private void buildCombinedTree(final ExpandSelectTreeNodeImpl root) throws EdmException {
-    for (SelectItem item : initialSelect) {
-      ExpandSelectTreeNodeImpl currentNode = root;
-      List<NavigationPropertySegment> segmentsList = item.getNavigationPropertySegments();
-      for (int segmentListIndex = 0; segmentListIndex < segmentsList.size(); segmentListIndex++) {
-        ExpandSelectTreeNodeImpl childNode = null;
-        //Check all expand lists for matches
-        for (ArrayList<NavigationPropertySegment> singleExpandList : initialExpand) {
-          if (singleExpandList.size() > segmentListIndex) {
-            boolean ok = true;
-            for (int expandListIndex = 0; expandListIndex <= segmentListIndex; expandListIndex++) {
-              if (!segmentsList.get(expandListIndex).getNavigationProperty().getName().equals(singleExpandList.get(expandListIndex).getNavigationProperty().getName())) {
-                ok = false;
-                break;
-              }
-            }
+  private void consolidateFalseNode(final ExpandSelectTreeNodeImpl node) {
+    Iterator<Map.Entry<String, ExpandSelectTreeNode>> iterator = node.getLinks().entrySet().iterator();
+    while (iterator.hasNext()) {
+      Map.Entry<String, ExpandSelectTreeNode> entry = iterator.next();
+      ExpandSelectTreeNodeImpl subNode = (ExpandSelectTreeNodeImpl) entry.getValue();
+      if (!subNode.isExpanded()) {
+        entry.setValue(null);
+      } else {
+        consolidate(subNode);
+      }
+    }
+  }
 
-            if (ok) {
-              childNode = new ExpandSelectTreeNodeImpl();
-              //If the whole navigation part of a select item matches expand everything if the expand still has segments left
-              if ((item.getProperty() == null && !item.isStar()) && segmentListIndex == segmentsList.size() - 1) {
-                ExpandSelectTreeNodeImpl expandNodes = childNode;
-                expandNodes.setAllExplicitly();
-                for (int expandListIndex = segmentListIndex + 1; expandListIndex < singleExpandList.size(); expandListIndex++) {
-                  ExpandSelectTreeNodeImpl newNode = new ExpandSelectTreeNodeImpl();
-                  newNode.setAllExplicitly();
-                  expandNodes = (ExpandSelectTreeNodeImpl) expandNodes.addChild(singleExpandList.get(expandListIndex).getNavigationProperty().getName(), newNode);
-                }
-              }
-              currentNode.addChild(segmentsList.get(segmentListIndex).getNavigationProperty().getName(), childNode);
-            }
-          }
+  private void consolidateTrueNode(final ExpandSelectTreeNodeImpl node) {
+    Iterator<Map.Entry<String, ExpandSelectTreeNode>> iterator = node.getLinks().entrySet().iterator();
+    while (iterator.hasNext()) {
+      Map.Entry<String, ExpandSelectTreeNode> entry = iterator.next();
+      ExpandSelectTreeNodeImpl subNode = (ExpandSelectTreeNodeImpl) entry.getValue();
+      if (subNode.isExpanded() && node.isExplicitlySeleced()) {
+        subNode.setExplicitlySeleced();
+        consolidate(subNode);
+      } else if (subNode.isExpanded()) {
+        consolidate(subNode);
+      } else {
+        iterator.remove();
+      }
+    }
+  }
+
+  private void createSelectTree(final ExpandSelectTreeNodeImpl root) throws EdmException {
+    for (SelectItem item : initialSelect) {
+      ExpandSelectTreeNodeImpl actualNode = root;
+
+      for (NavigationPropertySegment navSegement : item.getNavigationPropertySegments()) {
+        actualNode = addSelectNode(actualNode, navSegement.getNavigationProperty().getName());
+      }
+
+      if (item.getProperty() != null) {
+        actualNode.addProperty(item.getProperty());
+      } else if (item.isStar()) {
+        actualNode.setAllExplicitly();
+      } else {
+        //The actual node is a navigation property and has no property or star so it is explicitly selected
+        actualNode.setExplicitlySeleced();
+      }
+    }
+  }
+
+  private ExpandSelectTreeNodeImpl addSelectNode(final ExpandSelectTreeNodeImpl actualNode, final String navigationPropertyName) {
+    Map<String, ExpandSelectTreeNode> links = actualNode.getLinks();
+    if (!links.containsKey(navigationPropertyName)) {
+      ExpandSelectTreeNodeImpl subNode = new ExpandSelectTreeNodeImpl();
+      links.put(navigationPropertyName, subNode);
+      if (actualNode.isExplicitlySeleced()) {
+        //if a node was explicitly selected all sub nodes are explicitly selected
+        subNode.setExplicitlySeleced();
+      } else {
+        if (actualNode.getAllKind() == AllKinds.IMPLICITLYTRUE) {
+          actualNode.setAllKindFalse();
         }
-        currentNode = (ExpandSelectTreeNodeImpl) currentNode.addChild(segmentsList.get(segmentListIndex).getNavigationProperty().getName(), childNode);
-        if (currentNode == null) {
+      }
+      return subNode;
+    } else {
+      return (ExpandSelectTreeNodeImpl) links.get(navigationPropertyName);
+    }
+  }
+
+  private void mergeExpandTree(final ExpandSelectTreeNodeImpl root) throws EdmException {
+    for (ArrayList<NavigationPropertySegment> singleExpand : initialExpand) {
+      ExpandSelectTreeNodeImpl actualNode = root;
+      for (NavigationPropertySegment navSegement : singleExpand) {
+        actualNode = addExpandNode(actualNode, navSegement.getNavigationProperty().getName());
+        if (actualNode == null) {
           break;
         }
       }
+    }
 
-      if (currentNode != null) {
-        if (item.getProperty() != null) {
-          currentNode.addProperty(item.getProperty());
-        } else if (item.isStar()) {
-          currentNode.setAllExplicitly();
-        }
+  }
+
+  private ExpandSelectTreeNodeImpl addExpandNode(final ExpandSelectTreeNodeImpl actualNode, final String navigationPropertyName) {
+    Map<String, ExpandSelectTreeNode> links = actualNode.getLinks();
+    if (!links.containsKey(navigationPropertyName)) {
+      if (actualNode.isExplicitlySeleced() || (actualNode.isExplicitlySeleced() && actualNode.isExpanded())) {
+        ExpandSelectTreeNodeImpl subNode = new ExpandSelectTreeNodeImpl();
+        subNode.setExpanded();
+        subNode.setExplicitlySeleced();
+        links.put(navigationPropertyName, subNode);
+        return subNode;
+      } else {
+        return null;
       }
-
+    } else {
+      ExpandSelectTreeNodeImpl subNode = (ExpandSelectTreeNodeImpl) links.get(navigationPropertyName);
+      subNode.setExpanded();
+      return subNode;
     }
   }
+
 }
