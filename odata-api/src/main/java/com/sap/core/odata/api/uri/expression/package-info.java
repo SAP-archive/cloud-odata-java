40960476b3ea5b61497fc28f3633dd9c9a7f6d92
Christian Amend <christian.amend@sap.com>
2013-04-03 09:50:28 +0200

    Javadoc expression package in api
    Change-Id: I0a889c0768f311fe2012f5e1f636e3d8b33496fd

--- odata-api/src/main/java/com/sap/core/odata/api/uri/expression/package-info.java
+++ odata-api/src/main/java/com/sap/core/odata/api/uri/expression/package-info.java
@@ -1,4 +1,8 @@
 /**
- * <h1>Expression Parser</h1>
+ * Expression Parser
+ * <p>This package contains all classes necessary to decribe an expression tree(e.g. a filter or order by tree)
+ * 
+ * <p>Trees can be traversed by implementing the {@link com.sap.core.odata.api.uri.expression.ExpressionVisitor} interface and calling the accept() method. 
+ * <br>Different types of expressions can be found in {@link com.sap.core.odata.api.uri.expression.ExpressionKind}.
  */
 package com.sap.core.odata.api.uri.expression;
