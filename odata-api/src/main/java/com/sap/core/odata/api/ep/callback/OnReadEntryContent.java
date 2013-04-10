cac2f881eece7aa5f7d4529466fa8e94bd5c002d
Michael Bolz <michael.bolz@sap.com>
2013-04-10 15:32:36 +0200

    Changed 'Read callback' API and adopted implementation.
    Change-Id: I629aded463e86638733ca968b821a1ed58e55e6c

--- odata-api/src/main/java/com/sap/core/odata/api/ep/callback/OnReadEntryContent.java
+++ odata-api/src/main/java/com/sap/core/odata/api/ep/callback/OnReadEntryContent.java
@@ -2,8 +2,13 @@ package com.sap.core.odata.api.ep.callback;
 
 /**
  * Callback interface for the deep insert read calls (read of <code><m:inline></code> content). 
- * <p>If an inlined navigation property which points to an entry is found this callback will be called to retrieve the necessary data to read the inlined entity.
- * <br>Pointing to an entry means the navigation property has a multiplicity of <code>1..1</code> or <code>1..m</code>.
+ * <p>
+ * If an inlined navigation property which points to an entry or feed is found this callback will be called to deliver the read/de-serialized inlined entity.
+ * If inlined navigation property is <code>nullable</code> and not set a {@link ReadEntryResultContext} is given with the 
+ * <code>navigationPropertyName</code> and a <code>NULL</code> entry set.
+ * <br/>
+ * Pointing to an entry means the navigation property has a multiplicity of <code>0..1</code>, <code>1..1</code> or <code>1..m</code>.
+ * </p>
  * 
  * @author SAP AG
  *
@@ -11,11 +16,13 @@ package com.sap.core.odata.api.ep.callback;
 public interface OnReadEntryContent {
 
   /**
-   * All contextual information about inlined navigation property is given within {@link ReadEntryCallbackContext}.
-   * As result a {@link ReadCallbackResult} object is expected which <b>MUST NOT</b> be <code>NULL</code>.
+   * All contextual information and the de-serialized inlined navigation property is given within {@link ReadEntryResultContext}.
    * 
-   * @param callbackInfo with contextual information about inlined navigation property 
-   * @return a {@link ReadCallbackResult} object with all necessary data to read inlined entity (which <b>MUST NOT</b> be <code>NULL</code>).
+   * Additional an optional {@link ReadCallbackResult} object can be returned which then is added to the properties of the parent entry.
+   * If nothing (<code>NULL</code>) is returned nothing is added to the parent entry properties (<b>default behavior</b>).
+   * 
+   * @param callbackInfo with contextual information about and de-serialized inlined navigation property 
+   * @return a {@link ReadCallbackResult} optional result which content is added to parent entry properties.
    */
-  ReadCallbackResult retrieveReadResult(ReadEntryCallbackContext callbackInfo);
+  ReadCallbackResult handleReadResult(ReadEntryResultContext callbackInfo);
 }
