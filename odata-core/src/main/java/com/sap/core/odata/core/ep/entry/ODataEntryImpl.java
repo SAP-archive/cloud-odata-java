9f1ee70d3bd89e0660dbb5e112a749b5ff466a8e
Michael Bolz <michael.bolz@sap.com>
2013-04-18 15:46:45 +0200

    Minor code refactorings
    Change-Id: Id2a8a5b7b62c1a9f2a00393f6dcdf4c7dd4e9a5f

--- odata-core/src/main/java/com/sap/core/odata/core/ep/entry/ODataEntryImpl.java
+++ odata-core/src/main/java/com/sap/core/odata/core/ep/entry/ODataEntryImpl.java
@@ -13,21 +13,21 @@ import com.sap.core.odata.core.uri.ExpandSelectTreeNodeImpl;
  */
 public class ODataEntryImpl implements ODataEntry {
 
-  private Map<String, Object> data;
-  private EntryMetadata entryMetadata;
-  private MediaMetadata mediaMetadata;
-  private ExpandSelectTreeNode expandSelectTree;
+  private final Map<String, Object> data;
+  private final EntryMetadata entryMetadata;
+  private final MediaMetadata mediaMetadata;
+  private final ExpandSelectTreeNode expandSelectTree;
   private boolean containsInlineEntry;
 
-  public ODataEntryImpl(final Map<String, Object> data, final MediaMetadata mediaMetadata, final EntryMetadata entryMetadata) {
-    this(data, mediaMetadata, entryMetadata, false);
+  public ODataEntryImpl(final Map<String, Object> data, final MediaMetadata mediaMetadata, final EntryMetadata entryMetadata, ExpandSelectTreeNodeImpl expandSelectTree) {
+    this(data, mediaMetadata, entryMetadata, expandSelectTree, false);
   }
 
-  public ODataEntryImpl(final Map<String, Object> data, final MediaMetadata mediaMetadata, final EntryMetadata entryMetadata, final boolean containsInlineEntry) {
+  public ODataEntryImpl(final Map<String, Object> data, final MediaMetadata mediaMetadata, final EntryMetadata entryMetadata, ExpandSelectTreeNode expandSelectTree, final boolean containsInlineEntry) {
     this.data = data;
     this.entryMetadata = entryMetadata;
     this.mediaMetadata = mediaMetadata;
-    expandSelectTree = new ExpandSelectTreeNodeImpl();
+    this.expandSelectTree = expandSelectTree;
     this.containsInlineEntry = containsInlineEntry;
   }
 
@@ -58,10 +58,6 @@ public class ODataEntryImpl implements ODataEntry {
 
   public void setContainsInlineEntry(final boolean containsInlineEntry) {
     this.containsInlineEntry = containsInlineEntry;
-  }
-
-  public void setExpandSelectTree(final ExpandSelectTreeNode expandSelectTree) {
-    this.expandSelectTree = expandSelectTree;
   }
 
   @Override
