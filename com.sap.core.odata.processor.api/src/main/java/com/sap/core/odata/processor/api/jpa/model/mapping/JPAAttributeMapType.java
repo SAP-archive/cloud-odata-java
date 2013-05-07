ac6f62a83fe33cee3faebd6f0c43be0d5e7eff24
Santosh Kumar <santosh.kumar04@sap.com>
2013-05-07 14:11:00 +0530

    Changes for Mapping Enhancements.
    Change-Id: I95bcd6b869c70a18b70dee48ce156ff39747c70a
    Signed-off-by: Santosh Kumar <santosh.kumar04@sap.com>

--- com.sap.core.odata.processor.api/src/main/java/com/sap/core/odata/processor/api/jpa/model/mapping/JPAAttributeMapType.java
+++ com.sap.core.odata.processor.api/src/main/java/com/sap/core/odata/processor/api/jpa/model/mapping/JPAAttributeMapType.java
@@ -11,16 +11,15 @@ import javax.xml.bind.annotation.XmlValue;
 
 /**
  * 
- * The default name for EDM property is derived from JPA attribute name. This
- * can be overriden using JPAAttributeMapType.
+ * 				The default name for EDM
+ * 				property is derived from JPA attribute name. This can be overriden
+ * 				using
+ * 				JPAAttributeMapType.
+ * 			
  * 
+ * <p>Java class for JPAAttributeMapType complex type.
  * 
- * <p>
- * Java class for JPAAttributeMapType complex type.
- * 
- * <p>
- * The following schema fragment specifies the expected content contained within
- * this class.
+ * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
  * &lt;complexType name="JPAAttributeMapType">
@@ -32,6 +31,7 @@ import javax.xml.bind.annotation.XmlValue;
  *             &lt;simpleContent>
  *               &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
  *                 &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *                 &lt;attribute name="exclude" type="{http://www.w3.org/2001/XMLSchema}boolean" default="false" />
  *               &lt;/extension>
  *             &lt;/simpleContent>
  *           &lt;/complexType>
@@ -45,113 +45,151 @@ import javax.xml.bind.annotation.XmlValue;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "JPAAttributeMapType", propOrder = { "jpaAttribute" })
+@XmlType(name = "JPAAttributeMapType", propOrder = {
+    "jpaAttribute"
+})
 public class JPAAttributeMapType {
 
-	@XmlElement(name = "JPAAttribute")
-	protected List<JPAAttributeMapType.JPAAttribute> jpaAttribute;
+    @XmlElement(name = "JPAAttribute")
+    protected List<JPAAttributeMapType.JPAAttribute> jpaAttribute;
 
-	/**
-	 * Gets the value of the jpaAttribute property.
-	 * 
-	 * <p>
-	 * This accessor method returns a reference to the live list, not a
-	 * snapshot. Therefore any modification you make to the returned list will
-	 * be present inside the JAXB object. This is why there is not a
-	 * <CODE>set</CODE> method for the jpaAttribute property.
-	 * 
-	 * <p>
-	 * For example, to add a new item, do as follows:
-	 * 
-	 * <pre>
-	 * getJPAAttribute().add(newItem);
-	 * </pre>
-	 * 
-	 * 
-	 * <p>
-	 * Objects of the following type(s) are allowed in the list
-	 * {@link JPAAttributeMapType.JPAAttribute }
-	 * 
-	 * 
-	 */
-	public List<JPAAttributeMapType.JPAAttribute> getJPAAttribute() {
-		if (jpaAttribute == null) {
-			jpaAttribute = new ArrayList<JPAAttributeMapType.JPAAttribute>();
-		}
-		return this.jpaAttribute;
-	}
+    /**
+     * Gets the value of the jpaAttribute property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the jpaAttribute property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getJPAAttribute().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link JPAAttributeMapType.JPAAttribute }
+     * 
+     * 
+     */
+    public List<JPAAttributeMapType.JPAAttribute> getJPAAttribute() {
+        if (jpaAttribute == null) {
+            jpaAttribute = new ArrayList<JPAAttributeMapType.JPAAttribute>();
+        }
+        return this.jpaAttribute;
+    }
 
-	/**
-	 * <p>
-	 * Java class for anonymous complex type.
-	 * 
-	 * <p>
-	 * The following schema fragment specifies the expected content contained
-	 * within this class.
-	 * 
-	 * <pre>
-	 * &lt;complexType>
-	 *   &lt;simpleContent>
-	 *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
-	 *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
-	 *     &lt;/extension>
-	 *   &lt;/simpleContent>
-	 * &lt;/complexType>
-	 * </pre>
-	 * 
-	 * 
-	 */
-	@XmlAccessorType(XmlAccessType.FIELD)
-	@XmlType(name = "", propOrder = { "value" })
-	public static class JPAAttribute {
 
-		@XmlValue
-		protected String value;
-		@XmlAttribute(name = "name", required = true)
-		protected String name;
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;simpleContent>
+     *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
+     *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+     *       &lt;attribute name="exclude" type="{http://www.w3.org/2001/XMLSchema}boolean" default="false" />
+     *     &lt;/extension>
+     *   &lt;/simpleContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "value"
+    })
+    public static class JPAAttribute {
 
-		/**
-		 * Gets the value of the value property.
-		 * 
-		 * @return possible object is {@link String }
-		 * 
-		 */
-		public String getValue() {
-			return value;
-		}
+        @XmlValue
+        protected String value;
+        @XmlAttribute(name = "name", required = true)
+        protected String name;
+        @XmlAttribute(name = "exclude")
+        protected Boolean exclude;
 
-		/**
-		 * Sets the value of the value property.
-		 * 
-		 * @param value
-		 *            allowed object is {@link String }
-		 * 
-		 */
-		public void setValue(String value) {
-			this.value = value;
-		}
+        /**
+         * Gets the value of the value property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getValue() {
+            return value;
+        }
 
-		/**
-		 * Gets the value of the name property.
-		 * 
-		 * @return possible object is {@link String }
-		 * 
-		 */
-		public String getName() {
-			return name;
-		}
+        /**
+         * Sets the value of the value property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setValue(String value) {
+            this.value = value;
+        }
 
-		/**
-		 * Sets the value of the name property.
-		 * 
-		 * @param value
-		 *            allowed object is {@link String }
-		 * 
-		 */
-		public void setName(String value) {
-			this.name = value;
-		}
+        /**
+         * Gets the value of the name property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getName() {
+            return name;
+        }
 
-	}
+        /**
+         * Sets the value of the name property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setName(String value) {
+            this.name = value;
+        }
+
+        /**
+         * Gets the value of the exclude property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link Boolean }
+         *     
+         */
+        public boolean isExclude() {
+            if (exclude == null) {
+                return false;
+            } else {
+                return exclude;
+            }
+        }
+
+        /**
+         * Sets the value of the exclude property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link Boolean }
+         *     
+         */
+        public void setExclude(Boolean value) {
+            this.exclude = value;
+        }
+
+    }
 
 }
--- com.sap.core.odata.processor.api/src/main/java/com/sap/core/odata/processor/api/jpa/model/mapping/JPAEmbeddableTypeMapType.java
+++ com.sap.core.odata.processor.api/src/main/java/com/sap/core/odata/processor/api/jpa/model/mapping/JPAEmbeddableTypeMapType.java
@@ -8,16 +8,14 @@ import javax.xml.bind.annotation.XmlType;
 
 /**
  * 
- * The default name for EDM complex type is derived from JPA Embeddable type
- * name. This can be overriden using JPAEmbeddableTypeMapType.
+ * 				The default name for EDM
+ * 				complex type is derived from JPA Embeddable type name. This can be
+ * 				overriden using JPAEmbeddableTypeMapType.
+ * 			
  * 
+ * <p>Java class for JPAEmbeddableTypeMapType complex type.
  * 
- * <p>
- * Java class for JPAEmbeddableTypeMapType complex type.
- * 
- * <p>
- * The following schema fragment specifies the expected content contained within
- * this class.
+ * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
  * &lt;complexType name="JPAEmbeddableTypeMapType">
@@ -28,6 +26,7 @@ import javax.xml.bind.annotation.XmlType;
  *         &lt;element name="JPAAttributes" type="{http://www.sap.com/core/odata/processor/api/jpa/model/mapping}JPAAttributeMapType"/>
  *       &lt;/sequence>
  *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="exclude" type="{http://www.w3.org/2001/XMLSchema}boolean" default="false" />
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -36,78 +35,119 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "JPAEmbeddableTypeMapType", propOrder = { "edmComplexType",
-		"jpaAttributes" })
+@XmlType(name = "JPAEmbeddableTypeMapType", propOrder = {
+    "edmComplexType",
+    "jpaAttributes"
+})
 public class JPAEmbeddableTypeMapType {
 
-	@XmlElement(name = "EDMComplexType")
-	protected String edmComplexType;
-	@XmlElement(name = "JPAAttributes", required = true)
-	protected JPAAttributeMapType jpaAttributes;
-	@XmlAttribute(name = "name", required = true)
-	protected String name;
+    @XmlElement(name = "EDMComplexType")
+    protected String edmComplexType;
+    @XmlElement(name = "JPAAttributes", required = true)
+    protected JPAAttributeMapType jpaAttributes;
+    @XmlAttribute(name = "name", required = true)
+    protected String name;
+    @XmlAttribute(name = "exclude")
+    protected Boolean exclude;
 
-	/**
-	 * Gets the value of the edmComplexType property.
-	 * 
-	 * @return possible object is {@link String }
-	 * 
-	 */
-	public String getEDMComplexType() {
-		return edmComplexType;
-	}
+    /**
+     * Gets the value of the edmComplexType property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEDMComplexType() {
+        return edmComplexType;
+    }
 
-	/**
-	 * Sets the value of the edmComplexType property.
-	 * 
-	 * @param value
-	 *            allowed object is {@link String }
-	 * 
-	 */
-	public void setEDMComplexType(String value) {
-		this.edmComplexType = value;
-	}
+    /**
+     * Sets the value of the edmComplexType property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEDMComplexType(String value) {
+        this.edmComplexType = value;
+    }
 
-	/**
-	 * Gets the value of the jpaAttributes property.
-	 * 
-	 * @return possible object is {@link JPAAttributeMapType }
-	 * 
-	 */
-	public JPAAttributeMapType getJPAAttributes() {
-		return jpaAttributes;
-	}
+    /**
+     * Gets the value of the jpaAttributes property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link JPAAttributeMapType }
+     *     
+     */
+    public JPAAttributeMapType getJPAAttributes() {
+        return jpaAttributes;
+    }
 
-	/**
-	 * Sets the value of the jpaAttributes property.
-	 * 
-	 * @param value
-	 *            allowed object is {@link JPAAttributeMapType }
-	 * 
-	 */
-	public void setJPAAttributes(JPAAttributeMapType value) {
-		this.jpaAttributes = value;
-	}
+    /**
+     * Sets the value of the jpaAttributes property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JPAAttributeMapType }
+     *     
+     */
+    public void setJPAAttributes(JPAAttributeMapType value) {
+        this.jpaAttributes = value;
+    }
 
-	/**
-	 * Gets the value of the name property.
-	 * 
-	 * @return possible object is {@link String }
-	 * 
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * Sets the value of the name property.
-	 * 
-	 * @param value
-	 *            allowed object is {@link String }
-	 * 
-	 */
-	public void setName(String value) {
-		this.name = value;
-	}
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the exclude property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Boolean }
+     *     
+     */
+    public boolean isExclude() {
+        if (exclude == null) {
+            return false;
+        } else {
+            return exclude;
+        }
+    }
+
+    /**
+     * Sets the value of the exclude property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Boolean }
+     *     
+     */
+    public void setExclude(Boolean value) {
+        this.exclude = value;
+    }
 
 }
