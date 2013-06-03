f303540d7b365a9d4edd3ed6ea322cf27afd3136
Tamara Boehm <tamara.boehm@sap.com>
2013-06-03 12:44:00 +0200

    added keyword final to constants in EdmTargetPath
    Change-Id: Ia05b7b8b4486a17cee6561ee7db573fe0487430b

--- odata-api/src/main/java/com/sap/core/odata/api/edm/EdmTargetPath.java
+++ odata-api/src/main/java/com/sap/core/odata/api/edm/EdmTargetPath.java
@@ -7,16 +7,16 @@ package com.sap.core.odata.api.edm;
  */
 public class EdmTargetPath {
 
-  public static String SYNDICATION_AUTHORNAME = "SyndicationAuthorName";
-  public static String SYNDICATION_AUTHOREMAIL = "SyndicationAuthorEmail";
-  public static String SYNDICATION_AUTHORURI = "SyndicationAuthorUri";
-  public static String SYNDICATION_PUBLISHED = "SyndicationPublished";
-  public static String SYNDICATION_RIGHTS = "SyndicationRights";
-  public static String SYNDICATION_TITLE = "SyndicationTitle";
-  public static String SYNDICATION_UPDATED = "SyndicationUpdated";
-  public static String SYNDICATION_CONTRIBUTORNAME = "SyndicationContributorName";
-  public static String SYNDICATION_CONTRIBUTOREMAIL = "SyndicationContributorEmail";
-  public static String SYNDICATION_CONTRIBUTORURI = "SyndicationContributorUri";
-  public static String SYNDICATION_SOURCE = "SyndicationSource";
-  public static String SYNDICATION_SUMMARY = "SyndicationSummary";
+  public static final String SYNDICATION_AUTHORNAME = "SyndicationAuthorName";
+  public static final String SYNDICATION_AUTHOREMAIL = "SyndicationAuthorEmail";
+  public static final String SYNDICATION_AUTHORURI = "SyndicationAuthorUri";
+  public static final String SYNDICATION_PUBLISHED = "SyndicationPublished";
+  public static final String SYNDICATION_RIGHTS = "SyndicationRights";
+  public static final String SYNDICATION_TITLE = "SyndicationTitle";
+  public static final String SYNDICATION_UPDATED = "SyndicationUpdated";
+  public static final String SYNDICATION_CONTRIBUTORNAME = "SyndicationContributorName";
+  public static final String SYNDICATION_CONTRIBUTOREMAIL = "SyndicationContributorEmail";
+  public static final String SYNDICATION_CONTRIBUTORURI = "SyndicationContributorUri";
+  public static final String SYNDICATION_SOURCE = "SyndicationSource";
+  public static final String SYNDICATION_SUMMARY = "SyndicationSummary";
 }
