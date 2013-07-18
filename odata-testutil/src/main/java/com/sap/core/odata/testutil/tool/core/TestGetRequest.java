1c15a2f225548c8d7c44d795b3b4c8297e766196
Michael Bolz <michael.bolz@sap.com>
2013-07-18 13:08:03 +0200

    Removed 'performance test tool'
    Change-Id: Idccf7b5bac36dbf253cd54ec1f10384b4b9b925d

--- odata-testutil/src/main/java/com/sap/core/odata/testutil/tool/core/TestGetRequest.java
+++
@@ -1,9 +0,0 @@
-package com.sap.core.odata.testutil.tool.core;
-
-public class TestGetRequest extends TestRequest {
-
-  TestGetRequest(final String path, final int callCount) {
-    super(path, callCount);
-  }
-
-}
